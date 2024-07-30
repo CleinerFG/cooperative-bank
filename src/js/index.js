adf047d23de6d2a851b1fc0470346760623814fe
CleinerFG <cleinerfurlani@gmail.com>
2024-07-30 14:43:59 -0300

    Add visibility of total investments

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+import { visibilitySwitch } from "./modules/visibilitySwitch.js";
+
+// Menu
+
+// Visibility
+document
+  .querySelector(".visibility-icon")
+  .addEventListener("click", visibilitySwitch);
+
+// Wallet cards
+// Investments cards
+// Liquidy Events
---
+++ src/js/modules/visibilitySwitch.js
@@ -0,0 +1,14 @@
+export function visibilitySwitch(ev) {
+  const icon = ev.currentTarget;
+  const visibility = icon.dataset.visibility;
+
+  // Switching visibility and SVG file
+  const state = visibility === "off" ? "on" : "off";
+  icon.src = `assets/visibility-${state}.svg`;
+  icon.dataset.visibility = state;
+
+  // Showing the total value
+  // totalValue initially receives a test value, value logic is not implemented yet
+  let totalValue = state === "on" ? "12.954,53" : "******";
+  document.getElementById("total-value").textContent = totalValue;
+}
