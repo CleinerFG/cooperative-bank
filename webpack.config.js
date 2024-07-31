ad6c8483b64b041ffe632b6bd5c30ca9ab1b6edc
CleinerFG <cleinerfurlani@gmail.com>
2024-07-31 16:06:01 -0300

    Add optimization for all HTML pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,34 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
+const pages = [
+  { dir: "", files: ["index"] },
+  {
+    dir: "wallet",
+    files: ["debtors", "loans", "payments"],
+  },
+];
+
+function createHtmlPlugins({ pages }) {
+  return pages.flatMap(({ dir, files }) =>
+    files.map(
+      (file) =>
+        new HtmlWebpackPlugin({
+          template: `./src/pages/${dir}/${file}.html`,
+          filename: `${file}.html`,
+          minify: {
+            collapseWhitespace: true,
+            removeComments: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            useShortDoctype: true,
+          },
+        })
+    )
+  );
+}
+
 module.exports = {
   // Webpack base configs
   entry: { home: "./src/js/home.js" },
@@ -36,17 +64,6 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: "[name].bundle.min.css" }),
-    new HtmlWebpackPlugin({
-      template: "./src/pages/index.html",
-      filename: "index.html",
-      minify: {
-        collapseWhitespace: true,
-        removeComments: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        useShortDoctype: true,
-      },
-    }),
+    ...createHtmlPlugins({ pages }),
   ],
 };
