0c4168a9383e28e47e9b95bb7a562dd091463040
CleinerFG <cleinerfurlani@gmail.com>
2024-07-31 15:11:44 -0300

    Add HTML and CSS optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
+  // Webpack base configs
   entry: { home: "./src/js/home.js" },
   mode: "production", // development or production
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.min.js",
   },
+  // Loaders: babel-loader and mini-css-extract-plugin
   module: {
     rules: [
       {
@@ -23,5 +27,26 @@ module.exports = {
       },
     ],
   },
-  plugins: [new MiniCssExtractPlugin({ filename: "[name].bundle.min.css" })],
+  // Minification: CSS
+  optimization: {
+    minimizer: [
+      "...", // Default minimizers
+      new CssMinimizerPlugin(),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "[name].bundle.min.css" }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/index.html",
+      filename: "index.html",
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+  ],
 };
