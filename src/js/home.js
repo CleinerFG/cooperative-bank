b2830f5cb64cee09194314801591214190d3a9bc
CleinerFG <cleinerfurlani@gmail.com>
2024-08-05 15:43:29 -0300

    setSrc add in home.html

--- src/js/home.js
+++ src/js/home.js
@@ -1,4 +1,5 @@
 // import "../css/home.css";
+import "./modules/home/srcSetter.js";
 import { menuHandler } from "./modules/common/menuHandler.js";
 import { visibilitySwitch } from "./modules/home/visibilitySwitch.js";
 
---
+++ src/js/modules/home/srcSetter.js
@@ -0,0 +1,19 @@
+import { setSrc } from "../common/assetsPaths.js";
+
+// Menu
+setSrc(".menu-icon", "icons", "icon-menu.svg");
+
+// Financial Statement
+setSrc(".visibility-icon", "icons", "icon-visibility-off.svg");
+
+// Wallet
+setSrc("#card-icon-loans", "icons", "icon-loans.svg");
+setSrc("#card-icon-debtors", "icons", "icon-debtors.svg");
+setSrc("#card-icon-payments", "icons", "icon-payments.svg");
+
+// Investments
+setSrc("#card-icon-all", "icons", "icon-all.svg");
+setSrc("#card-icon-reports", "icons", "icon-reports.svg");
+
+// Footer
+setSrc(".footer-icon", "icons", "icon-globe.svg");
