2fc85eb0c6f803f1f2bf671ad0052a99f85a6a56
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2024-08-07 19:38:55 -0300

    Bugfix - background-color menu-icon

--- src/js/modules/components/header/menuHandler.js
+++ src/js/modules/components/header/menuHandler.js
@@ -1,7 +1,7 @@
 export function menuHandler() {
-  const menuIcon = document.querySelector(".menu-icon");
+  const menuIcon = document.querySelector(".menu-icon-container");
   menuIcon.addEventListener("click", (ev) => {
-    menuIcon.classList.toggle("menu-active");
+    menuIcon.classList.toggle("menu-active-container");
     const divOptions = document.querySelector(".menu-options");
     divOptions.classList.toggle("menu-options-block");
   });
