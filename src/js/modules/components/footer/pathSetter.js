011515391d709ebfd41de6932d56c8c10daec0a9
CleinerFG <cleinerfurlani@gmail.com>
2024-08-08 14:47:17 -0300

    Footer - BEM pattern

--- src/js/modules/components/footer/pathSetter.js
+++ src/js/modules/components/footer/pathSetter.js
@@ -2,7 +2,7 @@ import { setSrc } from "../../utils/path-manangers/assetsPaths.js";
 import { setHref } from "../../utils/path-manangers/htmlPagesPaths.js";
 
 export function initPathSettings() {
-  setSrc(".footer-icon", "icons", "icon-globe.svg");
-  setHref("#footer-icon-link", "home", "");
-  setHref("#footer-link", "home", "");
+  setSrc(".footer__icon", "icons", "icon-globe.svg");
+  setHref(".footer__brand-name", "home", "index.html");
+  setHref(".footer__icon-link", "home", "index.html");
 }
