444e49b91184f1eb8a78703197c58263f984d8be
CleinerFG <cleinerfurlani@gmail.com>
2024-08-13 11:28:28 -0300

    Restructuring insert footer

--- src/js/components/footer/createHtml.js
+++ src/js/components/footer/createHtml.js
@@ -1,7 +1,6 @@
 import { innerHTML } from "../../utils/dom/innerHTML.JS";
 
 const htmlStr = `
-<footer class="footer" aria-label="Footer">
   <a class="footer__icon-link">
     <img class="icon footer__icon" alt="Footer Icon">
   </a>
@@ -9,6 +8,5 @@ const htmlStr = `
     <a class="footer__brand-name">Coperative Bank</a>
     <p>By Cleiner Furlani</p>
   </div>
-</footer>
 `;
 export const createHtml = () => innerHTML("footer", htmlStr);
