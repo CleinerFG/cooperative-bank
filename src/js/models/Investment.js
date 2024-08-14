9cb65ee55d35d23ea1bedaf2ae946022ef91c3ef
CleinerFG <cleinerfurlani@gmail.com>
2024-08-14 15:27:58 -0300

    Create getHtmlStr method

--- src/js/models/Investment.js
+++ src/js/models/Investment.js
@@ -6,7 +6,28 @@ export class Investment extends Event {
     this.investmentType = investmentType;
   }
 
-  render() {
-    super.render();
+  getHtmlStr() {
+    return `
+    <article class="card card-data event__investment">
+      <header class="card-data__header">Investment</header>
+      <main class="card-data__content">
+        <div class="card-data__item">
+          <span class="card-data__label">Due date</span>
+          <span class="card-data__value">${this.dueDate}</span>
+        </div>
+        <div class="card-data__item">
+          <span class="card-data__label">Type</span>
+          <span class="card-data__value">${this.investmentType}</span>
+        </div>
+        <div class="card-data__item">
+          <span class="card-data__label">Value</span>
+          <span class="card-data__value">$ ${this.value}</span>
+        </div>
+      </main>
+      <footer class="card-data__footer">
+        <button class="btn card-data__btn">See</button>
+      </footer>
+    </article>
+    `;
   }
 }
---
+++ src/js/views/CardData.js
@@ -0,0 +1,18 @@
+import { insertHtml } from "./../utils/dom/insertHtml.js";
+import { Event } from "../models/Event.js";
+
+const inv = new Investment("10/05/2024", 500, "CDI");
+
+class CardData {
+  constructor(dataObject, parentNode) {
+    if (!(dataObject instanceof Event)) {
+      throw new Error("Object is not an instance of Event");
+    }
+    this.dataObject = dataObject;
+    this.parentNode = parentNode;
+  }
+
+  render(position) {
+    insertHtml(this.parentNode, position, this.dataObject.getHtmlStr());
+  }
+}
