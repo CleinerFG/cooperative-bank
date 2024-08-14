8f35680399dd151d35f2651fe53d578e28eea6d6
CleinerFG <cleinerfurlani@gmail.com>
2024-08-14 17:10:34 -0300

    EventController - Payments

--- src/js/models/Payment.js
+++ src/js/models/Payment.js
@@ -5,4 +5,29 @@ export class Payment extends Event {
     super("Payment", dueDate, value);
     this.creditor = creditor;
   }
+
+  getHtmlStr() {
+    return `
+    <article class="card card-data event__payment">
+      <header class="card-data__header">Payment</header>
+      <main class="card-data__content">
+        <div class="card-data__item">
+          <span class="card-data__label">Due date</span>
+          <span class="card-data__value">${this.dueDate}</span>
+        </div>
+        <div class="card-data__item">
+          <span class="card-data__label">Creditor</span>
+          <span class="card-data__value">${this.creditor}</span>
+        </div>
+        <div class="card-data__item">
+          <span class="card-data__label">Value</span>
+          <span class="card-data__value">$ ${this.value}</span>
+        </div>
+      </main>
+      <footer class="card-data__footer">
+        <button class="btn btn-attention card-data__btn">Pay</button>
+      </footer>
+    </article>
+    `;
+  }
 }
--- src/pages/home/scripts/eventsHandler.js
+++ src/pages/home/scripts/eventsHandler.js
@@ -1,4 +1,5 @@
 import { Investment } from "../../../js/models/Investment.js";
+import { Payment } from "../../../js/models/Payment.js";
 import { EventController } from "../../../js/controllers/EventController.js";
 
 const dataInvests = [
@@ -24,15 +25,34 @@ const dataInvests = [
   },
 ];
 
-export function initEventsHandler() {
-  const investInstances = dataInvests.map(
-    ({ dueDate, investmentType, value }) =>
-      new Investment(dueDate, value, investmentType)
-  );
+const dataPayments = [
+  { dueDate: "10/08/2024", value: "200", creditor: "Claudette Morel" },
+  { dueDate: "10/09/2024", value: "300", creditor: "Kate Denson" },
+  { dueDate: "10/10/2024", value: "50", creditor: "Meg Thomas" },
+];
 
+const investInstances = dataInvests.map(
+  ({ dueDate, investmentType, value }) =>
+    new Investment(dueDate, value, investmentType)
+);
+
+const payInstances = dataPayments.map(
+  ({ dueDate, value, creditor }) => new Payment(dueDate, value, creditor)
+);
+
+export function initEventsHandler() {
   const container = document.querySelector(".events__cards");
   const evController = new EventController(container);
 
-  investInstances.forEach((instance) => evController.addEvent(instance));
+  evController.addEvent(investInstances[0]);
+  evController.addEvent(payInstances[0]);
+  evController.addEvent(investInstances[1]);
+  evController.addEvent(investInstances[2]);
+  evController.addEvent(payInstances[1]);
+  evController.addEvent(investInstances[3]);
+  evController.addEvent(payInstances[2]);
+
+  // investInstances.forEach((instance) => evController.addEvent(instance));
+  // payInstances.forEach((instance) => evController.addEvent(instance));
   evController.renderAllEvents();
 }
