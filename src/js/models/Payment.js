c64124af83ff1c7addc154b97f8669e4e08628df
CleinerFG <cleinerfurlani@gmail.com>
2024-08-14 14:47:20 -0300

    Model Payment

---
+++ src/js/models/Payment.js
@@ -0,0 +1,12 @@
+import { Event } from "./Event.js";
+
+export class Payment extends Event {
+  constructor(dueDate, value, creditor) {
+    super("Payment", dueDate, value);
+    this.creditor = creditor;
+  }
+
+  render() {
+    super.render();
+  }
+}
