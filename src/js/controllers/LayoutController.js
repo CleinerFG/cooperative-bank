dd09cbc0bde9df63c63ec0f2946e0016bf92fa65
CleinerFG <cleinerfurlani@gmail.com>
2024-08-15 14:47:33 -0300

    Add ThemeView in the LayoutController

--- src/js/controllers/LayoutController.js
+++ src/js/controllers/LayoutController.js
@@ -1,14 +1,20 @@
 import { HeaderView } from "../views/components/HeaderView.js";
 import { FooterView } from "../views/components/FooterView.js";
+import { ThemeView } from "../views/components/ThemeView.js";
 
 export class LayoutController {
   constructor() {
     this.header = new HeaderView();
     this.footer = new FooterView();
+    this.theme = new ThemeView("coperativeBankTheme", "icon__theme-light");
   }
 
   initComponents() {
     this.header.render();
     this.footer.render();
+  }
+
+  initThemeSettings() {
+    this.theme.initializeTheme();
   }
 }
