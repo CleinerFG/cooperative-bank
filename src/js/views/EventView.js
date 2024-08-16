64006a368160f360bd6b0dabe68f8c17b75d2850
CleinerFG <cleinerfurlani@gmail.com>
2024-08-16 10:40:41 -0300

    Refactoring EventView

--- src/js/views/EventView.js
+++ src/js/views/EventView.js
@@ -8,19 +8,11 @@ export class EventView {
     return this._event;
   }
 
-  set event(value) {
-    this._event = value;
-  }
-
   get parentNode() {
     return this._parentNode;
   }
 
-  set parentNode(value) {
-    this._parentNode = value;
-  }
-
   render() {
-    this.parentNode.insertAdjacentHTML("afterbegin", this.event.getHtmlStr());
+    this._parentNode.insertAdjacentHTML("afterbegin", this.event.getHtmlStr());
   }
 }
