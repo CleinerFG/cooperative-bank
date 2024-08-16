a72388a22e7fce8f09e4332dcbb72fef61f79102
CleinerFG <cleinerfurlani@gmail.com>
2024-08-16 08:41:16 -0300

    References fixed in visibility function

--- src/pages/home/scripts/visibilityStatementValue.js
+++ src/pages/home/scripts/visibilityStatementValue.js
@@ -1,14 +1,19 @@
-import { setSrc } from "../../../js/utils/path-manangers/assetsPaths.js";
+import pathManager from "../../../js/utils/PathManager.js";
 // The asset/resource adds all the icons in dist, but the visibility-on icon needs to be imported directly into the JS. The html-loader removes it, because it is not used directly in the html
 // import "../../../assets/icons/icon-visibility-on.svg";
 
 export function switchVisibility() {
+  pathManager.updatePath(
+    "asset",
+    "#visibility-icon",
+    "icons",
+    `icon-visibility-off.svg`
+  );
   document
     .querySelector(".statement__visibility-button")
     .addEventListener("click", (ev) => {
       const button = ev.currentTarget;
       const visibility = button.dataset.visibility;
-
       // Alternative text
       const alt = visibility === "on" ? "Closed eye" : "Opened eye";
       document
@@ -17,7 +22,12 @@ export function switchVisibility() {
 
       // Switching visibility and SVG file
       const state = visibility === "off" ? "on" : "off";
-      setSrc("#visibility-icon", "icons", `icon-visibility-${state}.svg`);
+      pathManager.updatePath(
+        "asset",
+        "#visibility-icon",
+        "icons",
+        `icon-visibility-${state}.svg`
+      );
       button.dataset.visibility = state;
 
       // Showing the total value
