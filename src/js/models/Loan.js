bdd7f12abbcbc837a6694ef5bc4a378f95f4d70d
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2024-08-17 16:05:08 -0300

    Loan update

--- src/js/models/Loan.js
+++ src/js/models/Loan.js
@@ -1,16 +1,18 @@
 class Loan {
-  constructor(eventID, status, creditor, debtor, date, value, installments, interesRate) {
-    this._eventID = eventID;
+  constructor(loanID, status, description, debtor, creditor, date, value, installments, interesRate) {
+    this._loanID = loanID;
     this._status = status;
-    this._creditor = creditor;
+    this._description = description;
     this._debtor = debtor;
+    this._creditor = creditor;
     this._date = date;
     this._value = value;
     this._installments = installments;
     this._interestRate = interesRate;
     this._payments = [];
   }
-  _calculeInstallmentValue() {
+
+  calculeInstallmentValue() {
     return null;
   }
 
@@ -24,7 +26,7 @@ class Loan {
 
   getHtmlStr() {
     return `
-    <article class="card card-data">
+    <article id="${this._eventID}" class="card card-data">
           <header class="card-data__header">${this._status}</header>
           <main class="card-data__content">
             <div class="card-data__item">
@@ -45,7 +47,7 @@ class Loan {
             </div>
             <div class="card-data__item">
               <span class="card-data__label">Installment value</span>
-              <span class="card-data__value">$ ${this._calculeInstallmentValue()}</span>
+              <span class="card-data__value">$ ${this.calculeInstallmentValue()}</span>
             </div>
             <div class="card-data__item">
               <span class="card-data__label">Interest Rate</span>
