f63d81a8f328920a84bb059579ce7f469ba5b17d
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2024-08-17 17:21:23 -0300

    Model Loan update - getters, methods

--- src/js/models/Loan.js
+++ src/js/models/Loan.js
@@ -16,13 +16,72 @@ class Loan {
     this._value = value;
     this._installments = installments;
     this._interestRate = interesRate;
-    this._intallmentValue = this._calculeInstallmentValue();
+    this._installmentValue = this._calculeInstallmentValue();
     this._remainingInstallments = null;
     this._payments = [];
   }
 
+  // Getters
+  get loanID() {
+    return this._loanID;
+  }
+
+  get status() {
+    return loanStatus[this._status];
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  get debtor() {
+    return this._debtor;
+  }
+
+  get creditor() {
+    return this._creditor;
+  }
+
+  get date() {
+    return this._date;
+  }
+
+  get amountDue() {
+    return this._amountDue;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  get installments() {
+    return this._installments;
+  }
+
+  get interestRate() {
+    return this._interestRate;
+  }
+
+  get installmentValue() {
+    return this._installmentValue;
+  }
+
+  get remainingInstallments() {
+    return this._remainingInstallments;
+  }
+
+  get payments() {
+    return this._payments;
+  }
+
+  get totalLoanCost() {
+    return this._installmentValue * this._installments;
+  }
+
   _calculeInstallmentValue() {
-    return null;
+    const valueWithoutFees = (this._value / this._installments);
+    const fees = this._value * (this._interestRate / 100);
+    return valueWithoutFees + fees;
   }
 
   _updateRemaningInstallments() {
