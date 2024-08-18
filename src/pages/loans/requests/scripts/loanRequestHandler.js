3748ff63a36d046ded09ae1c5b917cb01024e6b4
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2024-08-18 15:06:23 -0300

    Add test data in the loans requests

--- src/pages/loans/requests/scripts/loanRequestHandler.js
+++ src/pages/loans/requests/scripts/loanRequestHandler.js
@@ -2,29 +2,67 @@ import { Loan } from "../../../../js/models/Loan.js";
 import { LoanRequest } from "../../../../js/models/LoanRequest.js";
 import { LoanRequestController } from "../../../../js/controllers/LoanRequestController.js";
 
-const loanData = {
-  loanId: 1334,
-  description: "Buy a Cell",
-  debtor: "Claudette",
-  creditor: "Kate",
-  value: 4000,
-  installments: 12,
-  interestRate: 2,
-};
+const loanData = [
+  {
+    loanId: 1334,
+    description: "Buy a Cell",
+    debtor: "Claudette",
+    creditor: "Kate",
+    value: 4000,
+    installments: 12,
+    interestRate: 2,
+  },
+  {
+    loanId: 1336,
+    description: "Buy food",
+    debtor: "Claudette",
+    creditor: "Vitorio",
+    value: 1100,
+    installments: 4,
+    interestRate: 2.1,
+  },
+  {
+    loanId: 1338,
+    description: "Credit card",
+    debtor: "Claudette",
+    creditor: "Kate",
+    value: 2000,
+    installments: 6,
+    interestRate: 2.5,
+  },
+];
 
-const request = {
-  requestID: 12,
-  date: "23/07/2024",
-};
+const requestData = [
+  {
+    requestID: 12,
+    date: "23/07/2024",
+  },
+  {
+    requestID: 14,
+    date: "03/09/2024",
+  },
+  {
+    requestID: 15,
+    date: "04/05/2024",
+  }
+];
 
-const loanObj = new Loan(loanData.loanId, loanData.description, loanData.debtor, loanData.creditor, loanData.value, loanData.installments, loanData.interestRate);
+const requests = [];
+for (let i = 0; i < requestData.length; i++) {
+  const loan = new Loan(loanData[i].loanId, loanData[i].description, loanData[i].debtor, loanData[i].creditor, loanData[i].value, loanData[i].installments, loanData[i].interestRate);
 
-const loanRequestObj = new LoanRequest(request.requestID, request.date, loanObj);
-loanRequestObj.status = 3;
+  const request = new LoanRequest(requestData[i].requestID, requestData[i].date, loan);
+
+  requests.push(request);
+}
+
+requests[0].status = 1;
+requests[1].status = 2;
+requests[2].status = 3;
 
 export function initLoanRequestController() {
   const container = document.querySelector(".open-requests__cards");
   const ctrl = new LoanRequestController(container);
-  ctrl.addLoanRequest(loanRequestObj);
+  requests.forEach((req) => ctrl.addLoanRequest(req))
   ctrl.renderRequests();
 }
