b862385543f74a7703c8bd5d91613da939ce74c4
CleinerFG <cleinerfurlani@gmail.com>
2024-08-19 14:10:56 -0300

    Update Event Controller

--- src/js/controllers/EventController.js
+++ src/js/controllers/EventController.js
@@ -2,14 +2,20 @@ import { ComponentController } from "./ComponentController.js";
 import { EventView } from "../views/EventView.js";
 
 export class EventController extends ComponentController {
-  constructor(container) {
+  #type;
+  constructor(container, type) {
     super(container, EventView);
+    this.#type = type;
   }
 
   _noComponentsHandler() {
     const t1 = "There are no events...";
     const t2 = "When there is news, we'll let you know ; )";
     super._noComponentsHandler(t1, t2);
+  }
+
+  renderComponent(view) {
+    view.render(this.#type);
   }
 
   removeComponent(eventID) {
