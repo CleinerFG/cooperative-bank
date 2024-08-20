f944b4afe77e1d671b89cea5d1fdc26be76e0f04
CleinerFG <cleinerfurlani@gmail.com>
2024-08-20 15:02:49 -0300

    improved Loan Request View

--- src/js/views/display/LoanRequestView.js
+++ src/js/views/display/LoanRequestView.js
@@ -5,57 +5,80 @@ export class LoanRequestView extends DisplayView {
     super(container, loanRequest);
   }
 
-  render() {
-    let cssClass = "pending";
-    let btnText = "Cancel Request";
+  #getCssClassAndButtonText() {
     switch (this.component.status) {
       case "Accepted":
-        cssClass = "success";
-        btnText = "Confirm";
-        break;
+        return { cssClass: "success", btnText: "Confirm" };
       case "Denied":
-        cssClass = "fail";
-        btnText = "Confirm";
+        return { cssClass: "fail", btnText: "Confirm" };
+      default:
+        return { cssClass: "pending", btnText: "Cancel Request" };
     }
+  }
 
-    let entity = this.component.creditor ? "Creditor" : "Debtor";
-    let entityValue = this.component.creditor ?? this.component.debtor;
-    btnText = this.component.debtor ? "Approve Loan" : btnText;
+  #getEntityInfo() {
+    const entity = this.component.creditor ? "Creditor" : "Debtor";
+    const entityValue = this.component.creditor ?? this.component.debtor;
+    return { entity, entityValue };
+  }
+
+  #getFooterStr() {
+    const footerOpened = `
+      <button class="btn btn-${
+        this.#getCssClassAndButtonText().cssClass
+      } card-data__btn">
+        ${this.#getCssClassAndButtonText().btnText}
+      </button>
+    `;
+    const footerReceived = `
+      <button class="btn btn-success card-data__btn">Approve Loan</button>
+      <button class="btn btn-fail card-data__btn">Recuse Loan</button>
+    `;
+    return this.component.debtor ? footerReceived : footerOpened;
+  }
+
+  #renderCardItem(label, value) {
+    return `
+      <div class="card-data__item">
+        <span class="card-data__label">${label}</span>
+        <span class="card-data__value">${value}</span>
+      </div>
+    `;
+  }
+
+  render() {
+    const { cssClass } = this.#getCssClassAndButtonText();
+    const { entity, entityValue } = this.#getEntityInfo();
+    const footerStr = this.#getFooterStr();
 
     const htmlStr = `
-    <article id="$request-${this.component.id}" class="card card-data request__${cssClass}">
-          <header class="card-data__header">${this.component.status}</header>
-          <main class="card-data__content">
-            <div class="card-data__item">
-              <span class="card-data__label">${entity}</span>
-              <span class="card-data__value">${entityValue}</span>
-            </div>
-            <div class="card-data__item">
-              <span class="card-data__label">Date</span>
-              <span class="card-data__value">${this.component.date}</span>
-            </div>
-            <div class="card-data__item">
-              <span class="card-data__label">Value</span>
-              <span class="card-data__value">$ ${this.component.value}</span>
-            </div>
-            <div class="card-data__item">
-              <span class="card-data__label">Installments</span>
-              <span class="card-data__value">${this.component.installments} months</span>
-            </div>
-            <div class="card-data__item">
-              <span class="card-data__label">Installment value</span>
-              <span class="card-data__value">$ ${this.component.installmentValue}</span>
-            </div>
-            <div class="card-data__item">
-              <span class="card-data__label">Interest Rate</span>
-              <span class="card-data__value">${this.component.rate}% p.m</span>
-            </div>
-          </main>
-          <footer class="card-data__footer">
-            <button class="btn btn-${cssClass} card-data__btn ">${btnText}</button>
-          </footer>
-        </article>
+      <article id="request-${
+        this.component.id
+      }" class="card card-data request__${cssClass}">
+        <header class="card-data__header">${this.component.status}</header>
+        <main class="card-data__content">
+          ${this.#renderCardItem(entity, entityValue)}
+          ${this.#renderCardItem("Date", this.component.date)}
+          ${this.#renderCardItem("Value", `$ ${this.component.value}`)}
+          ${this.#renderCardItem(
+            "Installments",
+            `${this.component.installments} months`
+          )}
+          ${this.#renderCardItem(
+            "Installment value",
+            `$ ${this.component.installmentValue}`
+          )}
+          ${this.#renderCardItem(
+            "Interest Rate",
+            `${this.component.rate}% p.m`
+          )}
+        </main>
+        <footer class="card-data__footer">
+          ${footerStr}
+        </footer>
+      </article>
     `;
+
     this.container.insertAdjacentHTML("afterbegin", htmlStr);
   }
 }
