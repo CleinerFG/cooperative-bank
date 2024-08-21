524f22582fad8fad4cb878c051751f497bd915d5
CleinerFG <cleinerfurlani@gmail.com>
2024-08-21 11:32:42 -0300

    Update Event, using superclass methods

--- src/js/views/display/EventView.js
+++ src/js/views/display/EventView.js
@@ -14,23 +14,31 @@ export class EventView extends DisplayView {
     return this.component.type === "payment" ? "Pay" : "See";
   }
 
+  #getLabelValue() {
+    return [
+      { label: "Due Date", value: this.component.dueDate },
+      { label: "Value", value: this.component.value },
+    ];
+  }
+
+  #getFooterStr() {
+    return `
+      <button class="btn ${this.#getBtnClass()} card-data__btn">
+      ${this.#getBtnText()}
+      </button>
+    `;
+  }
+
   render() {
-    const btnClass = this.#getBtnClass();
-    const btnText = this.#getBtnText();
     const capitalizedType = capitalize(this.component.type);
 
     const htmlStr = `
       <article id="event-${this.component.id}" class="card card-data event__${
       this.component.type
     }">
-        <header class="card-data__header">${capitalizedType}</header>
-        <main class="card-data__content">
-          ${this._renderCardItem("Due Date", this.component.dueDate)}
-          ${this._renderCardItem("Value", this.component.value)}
-        </main>
-        <footer class="card-data__footer">
-          <button class="btn ${btnClass} card-data__btn">${btnText}</button>
-        </footer>
+        ${this._createHeaderCard(capitalizedType)}
+        ${this._createMainCard(...this.#getLabelValue())}
+        ${this._createFooterCard(this.#getFooterStr())}
       </article>
       `;
     this.container.insertAdjacentHTML("afterbegin", htmlStr);
