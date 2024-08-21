58892ff6dde8a94e1c8e1ecdcbed6a5b0d49c835
CleinerFG <cleinerfurlani@gmail.com>
2024-08-21 14:21:42 -0300

    NoComponentsView update

---
+++ src/js/views/display/NoComponentsView.js
@@ -0,0 +1,37 @@
+export class NoComponentsView {
+  #container;
+  #texts;
+  constructor(container) {
+    this.#container = container;
+    this.#texts = [];
+  }
+
+  get texts() {
+    return this.#texts;
+  }
+
+  set texts(arrTexts) {
+    this.#texts = arrTexts;
+  }
+
+  #infoTexts() {
+    const createTagP = (txt) => `<p class="info-text">${txt}</p>`;
+    return arrTexts.map((txt) => createTagP(txt)).join("");
+  }
+
+  #createNoEventsContainer() {
+    return `
+    <div class="no-events">
+      <img class="no-events__img" id="no-events-img">
+      <div class="no-events__text">
+       ${this.#infoTexts()}
+      </div>
+    </div>
+    `;
+  }
+
+  render() {
+    const noEventsStr = this.#createNoEventsContainer();
+    this.#container.insertAdjacentHTML("afterbegin", noEventsStr);
+  }
+}
