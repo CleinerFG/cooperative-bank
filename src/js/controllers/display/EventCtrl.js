63ca234fb5b6b0b9d7d4e3163456f1ba7d243b08
CleinerFG <cleinerfurlani@gmail.com>
2024-08-21 16:13:44 -0300

    Default statement

--- src/js/controllers/display/EventCtrl.js
+++ src/js/controllers/display/EventCtrl.js
@@ -5,10 +5,9 @@ export class EventCtrl extends DisplayCtrl {
   constructor(container) {
     super(container, EventView);
   }
-  _defineNoComponentsSettings(
-    t1 = "There are no events...",
-    t2 = "When there is news, we'll let you know ; )"
-  ) {
+  _defineNoComponentsSettings() {
+    const t1 = "There are no events...";
+    const t2 = "When there is news, we'll let you know ; )";
     this.noComponentsCtrl.defineTexts(t1, t2);
   }
 }
