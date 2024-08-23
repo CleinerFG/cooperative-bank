e7b16bc40d544567b18c9fac0e05f12a90882e7f
CleinerFG <cleinerfurlani@gmail.com>
2024-08-23 15:57:38 -0300

    Test definition

--- src/pages/home/scripts/index.js
+++ src/pages/home/scripts/index.js
@@ -1,12 +1,21 @@
 // import "../css/home.css";
-import { initLayoutController } from "../../../js/controllers/core/layoutCore.js";
+import { HeaderCtrl } from "../../../js/controllers/layout/HeaderCtrl.js";
+import { FooterCtrl } from "../../../js/controllers/layout/FooterCtrl.js";
+import { ThemeView } from "../../../js/views/layout/ThemeView.js";
+
+// import { initLayoutController } from "../../../js/controllers/core/layoutCore.js";
 import { switchVisibility } from "./visibilityStatementValue.js";
 import { initActionCardsController } from "./actionCardsHandler.js";
 import { initPathManager } from "./pathHandler.js";
 import { initEventsController } from "./eventsHandler.js";
 
-initLayoutController();
+// initLayoutController();
 switchVisibility();
 initActionCardsController();
 initEventsController();
 initPathManager();
+
+// Test deinition
+new HeaderCtrl();
+new FooterCtrl();
+new ThemeView().initializeTheme();
