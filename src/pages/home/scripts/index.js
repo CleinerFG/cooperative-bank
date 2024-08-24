c2b86c37ad3904fc416d31cd325e2f5fb8150926
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2024-08-24 17:59:17 -0300

    Add changes

--- src/pages/home/scripts/index.js
+++ src/pages/home/scripts/index.js
@@ -1,13 +1,3 @@
-// import "../css/home.css";
-import { HomeView } from "../../../js/views/pages/homeView.js";
-import { switchVisibility } from "./visibilityStatementValue.js";
-// import { initActionCardsController } from "./actionCardsHandler.js";
-import { initPathManager } from "./pathHandler.js";
-import { initEventsController } from "./eventsHandler.js";
+import { HomeCtrl } from "../../../js/controllers/pages/HomeCtrl.js";
 
-// initLayoutController();
-new HomeView();
-switchVisibility();
-// initActionCardsController();
-initEventsController();
-initPathManager();
+new HomeCtrl();
