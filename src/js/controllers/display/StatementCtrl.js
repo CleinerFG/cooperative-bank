16996323ae0489e5d0eb7b5ebd6926dfc1019ce2
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2024-08-24 10:36:26 -0300

    Update controller

--- src/js/controllers/display/StatementCtrl.js
+++ src/js/controllers/display/StatementCtrl.js
@@ -3,24 +3,25 @@ import pathUtil from "../../utils/PathManager.js";
 
 export class StatementCtrl {
   #container;
-  #pathManager;
   constructor(container) {
     this.#container = container;
-    this.#pathManager = pathUtil;
+    this.pathManager = pathUtil;
     this.#init();
   }
 
-  #pathHandler() {
-    this.#pathManager.updatePath(
+  pathHandler(state = "off") {
+    this.pathManager.updatePath(
       "asset",
       "#visibility-icon",
       "icons",
-      "icon-visibility-off.svg"
+      `icon-visibility-${state}.svg`
     );
   }
 
   #init() {
     const view = new StatementView(this.#container);
-    this.#pathHandler();
+    this.pathHandler();
+    view.render();
+    view.switchVisibility(this.pathHandler.bind(this))
   }
 }
--- src/js/views/display/StatementView.js
+++ src/js/views/display/StatementView.js
@@ -17,6 +17,30 @@ export class StatementView {
     `
   }
 
+  switchVisibility(updatePath) {
+    document
+      .querySelector(".statement__visibility-button")
+      .addEventListener("click", (ev) => {
+        const button = ev.currentTarget;
+        const visibility = button.dataset.visibility;
+        // Alternative text
+        const alt = visibility === "on" ? "Closed eye" : "Opened eye";
+        document
+          .querySelector(".statement__visibility-icon")
+          .setAttribute("alt", alt);
+
+        // Switching visibility and SVG file
+        const state = visibility === "off" ? "on" : "off";
+        updatePath(state);
+        button.dataset.visibility = state;
+
+        // Showing the total value
+        let totalValue = state === "on" ? "12.954,53" : "******";
+        document.getElementById("total-value").textContent = totalValue;
+      });
+  }
+
+
   render() {
     this.#container.insertAdjacentHTML("afterbegin", this.#create());
   }
