636adaa90b31a84db71d1f2077451e2d6564b216
CleinerFG <cleinerfurlani@gmail.com>
2024-09-03 17:08:28 -0300

    Add examples

--- src/js/utils/stringUtils.js
+++ src/js/utils/stringUtils.js
@@ -1,7 +1,9 @@
+// "javascript" - type string -> "Javascript" - type string
 export function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+// "R$ 4.854,98" - type string -> 4854.98 - type Number
 export function monetaryValueToNumber(str) {
   const numberStr = str
     .replace(/[R$\s]/g, "")
