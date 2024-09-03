dd2379f224e3c34a0624bc5bfd814a5147fec06e
CleinerFG <cleinerfurlani@gmail.com>
2024-09-03 17:05:18 -0300

    Create monetary value to number

--- src/js/utils/stringUtils.js
+++ src/js/utils/stringUtils.js
@@ -1,3 +1,11 @@
 export function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function monetaryValueToNumber(str) {
+  const numberStr = str
+    .replace(/[R$\s]/g, "")
+    .replace(/\./g, "")
+    .replace(",", ".");
+  return Number(numberStr);
+}
