7f44288f7a037bc908dd76795e9ce318c92e1177
CleinerFG <cleinerfurlani@gmail.com>
2024-09-04 15:50:30 -0300

    Created InputCtrl base

---
+++ src/js/controllers/forms/InputCtrl.js
@@ -0,0 +1,22 @@
+import { AbstractMethodError } from "../../errors/AbstractMethodError.js";
+import { InputView } from "../../views/forms/InputView";
+
+export class InputCtrl {
+  constructor(container, id, labelText, placeholder, type) {
+    this._view = new InputView(container, id, labelText, placeholder, type);
+  }
+
+  _build() {
+    throw new AbstractMethodError("build");
+  }
+
+  _defineValidations() {
+    throw new AbstractMethodError("defineValidations");
+  }
+
+  init() {
+    this._build();
+    this._view.render();
+    this._defineValidations();
+  }
+}
