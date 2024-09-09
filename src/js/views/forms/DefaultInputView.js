e809266474f665ba1e782637b20c62304bafc69c
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2024-09-08 21:44:59 -0300

    define subclass

---
+++ src/js/views/forms/DefaultInputView.js
@@ -0,0 +1,15 @@
+import { InputView } from "./InputView.js";
+
+export class DefaultInputView extends InputView {
+  _htmlStr =  `
+  <div class="form-group__input-group">
+    <label for="${this._id}" class="label form-group__label">${this.#labelText
+  }</label>
+    <input id="${this._id}" type="text" inputmode="${this.#inputmode}" name="${this._id
+  }" placeholder="${this.#placeholder}" aria-label="${this.#labelText}"
+      class="input form-group__input">
+    <span id="${this._id}-error"></span>
+  </div>`
+  #build{
+  }
+}
