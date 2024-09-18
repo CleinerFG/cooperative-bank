42252b6184b9f6a81d4326a336c7afb7e0d95853
CleinerFG <cleinerfurlani@gmail.com>
2024-09-18 16:24:57 -0300

    Created test window modal

--- src/test/scripts/index.js
+++ src/test/scripts/index.js
@@ -1,74 +1,15 @@
+// Obter elementos
+const modal = document.getElementById('modal');
+const openModalBtn = document.getElementById('openModalBtn');
+const closeModalBtn = document.querySelector('.close-btn');
 
+// Abrir modal ao clicar no botão
+openModalBtn.addEventListener('click', () => {
+    modal.style.display = 'flex';
+});
 
-class SearchInputView {
-  #researchData;
-  #dataset;
-  constructor(input, resultList, dataset, researchData) {
-    this.input = input;
-    this.resultList = resultList;
-    this.#dataset = dataset;
-    this.#researchData = researchData;
-    this.init();
-  }
+// Fechar modal ao clicar no 'X'
+closeModalBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
 
-  #getElementDataset(element) {
-    return element.dataset[`${this.#dataset}Id`];
-  }
-
-  #setElementDataset(element, value) {
-    element.dataset[`${this.#dataset}Id`] = value;
-  }
-
-  #filterList(query) {
-    const normalizedQuery = query.toLowerCase();
-    return this.#researchData.filter(({ name }) =>
-      name.toLowerCase().includes(normalizedQuery)
-    );
-  }
-
-  #handleItemClick = (event) => {
-    const selectedItem = event.target;
-    this.#setElementDataset(input, this.#getElementDataset(selectedItem));
-    this.input.value = selectedItem.textContent;
-    this.resultList.innerHTML = "";
-  };
-
-  #renderItems(listItems) {
-    this.resultList.innerHTML = "";
-    listItems.forEach(({ id, name }) => {
-      const li = document.createElement("li");
-
-      li.className = "query-item";
-      this.#setElementDataset(li, id);
-      li.textContent = name;
-
-      li.addEventListener("click", this.#handleItemClick);
-      this.resultList.appendChild(li);
-    });
-  }
-
-  #handleInput = (event) => {
-    const query = event.target.value;
-    const filteredList = this.#filterList(query);
-    this.#renderItems(filteredList);
-  };
-
-  #clearItemsOnBlur = () => {
-    setTimeout(() => {
-      this.resultList.innerHTML = "";
-    }, 200);
-  };
-
-  init() {
-    this.input.addEventListener("click", () =>
-      this.#renderItems(this.#researchData)
-    );
-    this.input.addEventListener("input", this.#handleInput);
-    this.input.addEventListener("blur", this.#clearItemsOnBlur);
-  }
-}
-
-const input = document.querySelector("#creditor");
-const resultList = document.querySelector("#creditor-result");
-
-new SearchInput(input, resultList, "creditor", users);
