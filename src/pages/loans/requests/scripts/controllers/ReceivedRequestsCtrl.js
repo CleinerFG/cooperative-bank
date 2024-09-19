b069b6b92717a511067be597e64ca444ad89a140
CleinerFG <cleinerfurlani@gmail.com>
2024-09-19 14:28:10 -0300

    Implement received requests controller

---
+++ src/pages/loans/requests/scripts/controllers/ReceivedRequestsCtrl.js
@@ -0,0 +1,15 @@
+import { ComponentCtrl } from "../../../../../js/controllers/ComponentCtrl.js";
+import { ReceivedRequestView } from "../views/ReceivedRequestsView.js";
+
+export class ReceivedRequestsCtrl extends ComponentCtrl {
+  constructor() {
+    const container = document.querySelector(".received-requests__cards");
+    super(container, ReceivedRequestView, "received");
+  }
+
+  _defineNoComponentsSettings() {
+    super._defineNoComponentsSettings();
+    const t1 = `There are no received requests...`;
+    this.noComponentsCtrl.defineTexts(t1);
+  }
+}
