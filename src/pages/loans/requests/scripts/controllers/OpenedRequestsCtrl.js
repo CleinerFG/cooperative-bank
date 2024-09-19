5ad4e53090a03e620bd8724d01071f5d0310ba34
CleinerFG <cleinerfurlani@gmail.com>
2024-09-19 14:24:54 -0300

    Created opened requests controller

---
+++ src/pages/loans/requests/scripts/controllers/OpenedRequestsCtrl.js
@@ -0,0 +1,15 @@
+import { ComponentCtrl } from "../../../../../js/controllers/ComponentCtrl.js";
+import { OpenedRequestView } from "../views/OpenedRequestView.js";
+
+export class OpenedRequestsCtrl extends ComponentCtrl {
+  constructor() {
+    const container = document.querySelector(".open-requests__cards");
+    super(container, OpenedRequestView, "opened");
+  }
+
+  _defineNoComponentsSettings() {
+    super._defineNoComponentsSettings();
+    const t1 = `There are no opened requests...`;
+    this.noComponentsCtrl.defineTexts(t1);
+  }
+}
