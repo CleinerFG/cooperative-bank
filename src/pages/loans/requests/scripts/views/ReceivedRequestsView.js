cd8d7cc2cb07a8f9a0a858f61c35134a4ef430c5
CleinerFG <cleinerfurlani@gmail.com>
2024-09-19 14:20:46 -0300

    Created Received request View

---
+++ src/pages/loans/requests/scripts/views/ReceivedRequestsView.js
@@ -0,0 +1,54 @@
+import { LoanRequestView } from "./LoanRequestView.js";
+
+export class ReceivedRequestView extends LoanRequestView {
+  get approveBtnElement() {
+    return document.getElementById(
+      `btn-received-request-approve-${this.componentModel.id}`
+    );
+  }
+
+  get recuseBtnElement() {
+    return document.getElementById(
+      `btn-received-request-recuse-${this.componentModel.id}`
+    );
+  }
+
+  get _cssId() {
+    return `request-received-${this.componentModel.id}`;
+  }
+
+  get _cssClass() {
+    return `request__pending`;
+  }
+
+  get _labelValue() {
+    return [
+      { label: "Debtor", value: this.componentModel.debtor },
+      { label: "Date", value: this.componentModel.date },
+      { label: "Value", value: this.componentModel.value },
+      { label: "Description", value: this.componentModel.description },
+      { label: "Installments", value: this.componentModel.installments },
+      {
+        label: "Installment Value",
+        value: this.componentModel.installmentValue,
+      },
+      { label: "Interest Rate", value: this.componentModel.rate },
+    ];
+  }
+
+  get _headerCard() {
+    return this._createHeaderCard(this.componentModel.status);
+  }
+
+  get _mainCard() {
+    return this._createMainCard(...this._labelValue);
+  }
+
+  get _footerCard() {
+    const str = `
+      <button id="btn-received-request-approve-${this.componentModel.id}" class="btn btn-success card-data__btn">Approve</button>
+      <button id="btn-received-request-recuse-${this.componentModel.id}" class="btn btn-fail card-data__btn">Recuse</button>
+    `;
+    return this._createFooterCard(str);
+  }
+}
