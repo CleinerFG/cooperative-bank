2d164b1b60022ce61ee79a8d920d58e38ae77657
CleinerFG <cleinerfurlani@gmail.com>
2024-09-23 17:30:04 -0300

    Add listener to switch visibility

--- src/js/views/forms/SwitchVisibilityInputView.js
+++ src/js/views/forms/SwitchVisibilityInputView.js
@@ -7,8 +7,10 @@ export class SwitchVisibilityInputView extends InputView {
       <label for="${this._id}" class="label form-group__label">${this._labelText}</label>
       <div class="input__container">
         <input id="${this._id}" type="${this._type}" inputmode="${this._inputmode}" name="${this._id}" placeholder="${this._placeholder}" aria-label="${this._labelText}"
-        class="input form-group__input">
-        <button id="${this._id}-visibility">Switch</button>
+        class="input form-group__input" data-visibility="off">
+        <button id="${this._id}-visibility">
+          <img id="visibility-icon" class="icon inp__visibility-icon" alt="Closed eye">
+        </button>
       </div>
       <span id="${this._id}-error" class="error-message"></span>
     </div>`;
@@ -20,8 +22,28 @@ export class SwitchVisibilityInputView extends InputView {
     this._inputElement.type = type;
   }
 
+  _switchVisibility(ev, assetHandler) {
+    // const btn = document.querySelector(`${this._id}-visibility"`);
+    const btn = ev.currentTarget;
+    btn.addEventListener("click", () => {
+      const visibility = this._inputElement.dataset.visibility;
+      // Alternative text
+      const alt = visibility === "on" ? "Closed eye" : "Opened eye";
+      document.querySelector(".inp__visibility-icon").setAttribute("alt", alt);
+
+      // Switching visibility and SVG file
+      const state = visibility === "off" ? "on" : "off";
+      // assetHandler(state);
+      this._inputElement.dataset.visibility = state;
+    });
+  }
+
   _listenersHandler() {
     const btnSwitch = document.querySelector(`#${this._id}-visibility`);
-    btnSwitch.addEventListener("click", this._toggleInpType.bind(this));
+    btnSwitch.addEventListener("click", (ev) => {
+      // this._toggleInpType.bind(this);
+      this._toggleInpType();
+      this._switchVisibility(ev, undefined);
+    });
   }
 }
