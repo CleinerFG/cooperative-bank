3174a761ac8e94372fa638c03d466bcb47e9bd15
CleinerFG <cleinerfurlani@gmail.com>
2024-09-24 16:21:52 -0300

    Removed old const

--- src/js/views/layout/FooterView.js
+++ src/js/views/layout/FooterView.js
@@ -1,17 +1,6 @@
 import { LayoutView } from "./LayoutView.js";
 import { PathManager } from "../../utils/PathManager.js";
 
-const htmlStr = `  
-<footer id="footer" class="footer" aria-label="Footer">
-<a class="footer__icon-link">
-  <img class="icon footer__icon" alt="Footer Icon">
-</a>
-<div class="footer__content">
-  <a class="footer__brand-name">Coperative Bank</a>
-  <p>By Cleiner Furlani</p>
-</div>
-</footer>`;
-
 export class FooterView extends LayoutView {
   get _htmlStr() {
     return `  
