e793b6432dbf2f9d87556f2643d6f61693ad55c1
CleinerFG <cleinerfurlani@gmail.com>
2024-10-02 11:03:05 -0300

    Implement Loading card view and css

---
+++ src/js/views/LoadingCardView.js
@@ -0,0 +1,37 @@
+export class LoadingCardView {
+  #container;
+  constructor(container) {
+    this.#container = container;
+  }
+
+  get #template() {
+    return `
+    <article class="card card-data loading">
+      <header class="card-data__header"></header>
+      <main class="card-data__content">
+        <div class="card-data__item">
+          <span class="card-data__label"></span>
+          <span class="card-data__value"></span>
+        </div>
+        <div class="card-data__item">
+          <span class="card-data__label"></span>
+          <span class="card-data__value"></span>
+        </div>
+        <div class="card-data__item">
+          <span class="card-data__label"></span>
+          <span class="card-data__value"></span>
+        </div>
+      </main>
+      <footer class="card-data__footer"></footer>
+    </article>
+    `;
+  }
+
+  render() {
+    this.#container.insertAdjacentHTML("beforend", this.#template);
+  }
+
+  remove() {
+    this.#container.querySelector(".card-data.loading").remove();
+  }
+}
