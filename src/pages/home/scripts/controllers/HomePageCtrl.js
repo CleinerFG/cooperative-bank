c1d873bd189c82dd5666812235e17984d6329e4d
CleinerFG <cleinerfurlani@gmail.com>
2024-10-02 11:50:48 -0300

    Improvement home page view and controller

--- src/pages/home/scripts/controllers/HomePageCtrl.js
+++ src/pages/home/scripts/controllers/HomePageCtrl.js
@@ -1,32 +1,17 @@
 import { PageCtrl } from "../../../../js/controllers/PageCtrl.js";
-import { HomeView } from "../views/HomeView.js";
+import { HomePageView } from "../views/HomePageView.js";
 import { StatementCtrl } from "../controllers/StatementCtrl.js";
 import { ActionCardsCtrl } from "../controllers/ActionCardsCtrl.js";
 import { EventsCtrl } from "./EventsCtrl.js";
 
 export class HomePageCtrl extends PageCtrl {
   constructor() {
-    super(HomeView);
+    super(HomePageView);
   }
-
-  _initStatementCtrl() {
-    new StatementCtrl();
-  }
-
-  _initActionCardsCtrl() {
-    new ActionCardsCtrl();
-  }
-
-  _initEventsCtrl() {
-    const ctrl = new EventsCtrl();
-    // ctrl.initComponents();
-    // ctrl.removeComponent(1450);
-    // ctrl.clearComponents();
-  }
-
+  
   _initControllers() {
-    this._initStatementCtrl();
-    this._initActionCardsCtrl();
-    this._initEventsCtrl();
+    new StatementCtrl();
+    new ActionCardsCtrl();
+    new EventsCtrl();
   }
 }
--- src/pages/home/scripts/views/HomePageView.js
+++ src/pages/home/scripts/views/HomePageView.js
@@ -1,8 +1,8 @@
 import { PageView } from "../../../../js/views/PageView.js";
 import { capitalize } from "../../../../js/utils/stringUtils.js";
 
-export class HomeView extends PageView {
-  _createStatement() {
+export class HomePageView extends PageView {
+  _buidStatement() {
     return `
     <section class="section statement">
       <h1 class="section__h1 statement__title">Financial Statement</h1>
@@ -11,7 +11,7 @@ export class HomeView extends PageView {
     `;
   }
 
-  _createSection(name) {
+  _buildActionSection(name) {
     return `
     <section class="section ${name}">
       <h2 class="section__h2">${capitalize(name)}</h2>
@@ -23,15 +23,29 @@ export class HomeView extends PageView {
     `;
   }
 
-  _createAllSections() {
-    const sections = ["loans", "investments", "events"];
-    return sections.map((sec) => this._createSection(sec)).join("");
+  _buildEventsSection(name) {
+    return `
+    <section class="section events">
+      <h2 class="section__h2">Events</h2>
+      <div class="cards events__cards">
+      </div>
+    </section>
+    `;
+  }
+
+  _build() {
+    const actionSections = ["loans", "investments"];
+    const statement = this._buidStatement();
+    const actions = actionSections
+      .map((sec) => this._buildActionSection(sec))
+      .join("");
+    const events = this._buildEventsSection();
+    return statement + actions + events;
   }
 
   _pageContent() {
     const content = `
-    ${this._createStatement()}
-    ${this._createAllSections()}
+      ${this._build()}
     `;
     return content;
   }
