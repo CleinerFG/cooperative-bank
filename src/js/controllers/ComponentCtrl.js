37170df8326143571538fa527b691b75c4a828d9
CleinerFG <cleinerfurlani@gmail.com>
2024-10-08 14:53:43 -0300

    Create Component Controller

---
+++ src/js/controllers/ComponentCtrl.js
@@ -0,0 +1,41 @@
+import { AbstractMethodError } from "../errors/AbstractMethodError.js";
+
+export class ComponentCtrl {
+  #containerElement;
+  #modelParams;
+  #modelInstance;
+  #viewInstance;
+  constructor(containerElement, modelParams) {
+    this.#containerElement = containerElement;
+    this.#modelParams = modelParams;
+    this.#init();
+  }
+
+  get _ViewClass() {
+    throw new AbstractMethodError("_ViewClass");
+  }
+
+  get _ModelClass() {
+    throw new AbstractMethodError("_ModelClass");
+  }
+
+  get _containerElement() {
+    return this.#containerElement;
+  }
+
+  get _modelInstance() {
+    return this.#modelInstance;
+  }
+
+  get _viewInstance() {
+    return this.#viewInstance;
+  }
+
+  #init() {
+    this.#modelInstance = new this._ModelClass(this.#modelParams);
+    this.#viewInstance = new this._ViewClass(
+      this.#containerElement,
+      this.#modelInstance
+    );
+  }
+}
