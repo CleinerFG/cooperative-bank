5fbf60ef2642dfb48cd2b1874c402cbf33cd2a23
CleinerFG <cleinerfurlani@gmail.com>
2024-10-25 15:29:38 -0300

    Add swiper instance

---
+++ src/pages/home/scripts/components/swiper.js
@@ -0,0 +1,9 @@
+// Test implement in the page
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  freeMode: true,
+  grabCursor: true,
+});
