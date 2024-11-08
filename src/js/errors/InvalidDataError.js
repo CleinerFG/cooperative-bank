308e2d5768dce211aac7d8685ad716a5459e4ede
CleinerFG <cleinerfurlani@gmail.com>
2024-11-08 16:35:55 -0300

    Add doc - Error classes

--- src/js/errors/InvalidDataError.js
+++ src/js/errors/InvalidDataError.js
@@ -1,3 +1,7 @@
+/**
+ * Error class for handling invalid data.
+ * Thrown when the provided data is invalid.
+ */
 export class InvalidDataError extends Error {
   constructor() {
     super("Invalid Data Error");
