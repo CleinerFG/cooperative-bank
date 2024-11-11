27a0f34fbd507414fc5063dd3a84ba2bf63229e1
CleinerFG <cleinerfurlani@gmail.com>
2024-11-11 16:51:41 -0300

    Add doc - OverviewPageView

--- src/pages/loans/overview/scripts/views/OverviewPageView.js
+++ src/pages/loans/overview/scripts/views/OverviewPageView.js
@@ -1,6 +1,13 @@
-import { PageView } from "../../../../../js/views/PageView.js";
+import { PageView } from '../../../../../js/views/PageView.js';
 
 export class OverviewPageView extends PageView {
+  /**
+   * Returns the HTML string for the section displaying an overview of active loans.
+   * Provides an introductory message describing active loans.
+   *
+   * @protected
+   * @type {string}
+   */
   get _templateSectionInitial() {
     return `
     <section class="section active-loans">
@@ -10,12 +17,20 @@ export class OverviewPageView extends PageView {
     `;
   }
 
+  /**
+   * Returns the HTML string for the section displaying the total loan report.
+   * Contains placeholders for total amounts to pay and to receive.
+   *
+   * @protected
+   * @type {string}
+   * @note Total values - Must be implemented
+   */
   get _templateSectionTotalReport() {
     return `
     <section class="section total-report">
       <h2 class="section__h2">Total Report</h2>
-      <p class="info-text">Total to Pay: "Must be implemented"</p>
-      <p class="info-text">Total to Receive: "Must be implemented"</p>
+      <p class="info-text">Total to Pay: R$ 4.576,00</p>
+      <p class="info-text">Total to Receive: R$ 15.454,68</p>
     </section>
     `;
   }
