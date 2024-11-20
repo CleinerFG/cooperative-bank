135a698166da876224740d6cd4c852e547611d3d
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2024-11-20 15:14:12 -0300

    Removed swiper

--- src/pages/home/scripts/components/swiper.js
+++
@@ -1,9 +0,0 @@
-// Test implement in the page
-const swiper = new Swiper(".swiper", {
-  direction: "horizontal",
-  loop: false,
-  slidesPerView: "auto",
-  spaceBetween: 16,
-  freeMode: true,
-  grabCursor: true,
-});
