05f6cb4ba0159e3393e101da9d7fb0e72cbd6200
CleinerFG <cleinerfurlani@gmail.com>
2024-11-21 16:18:55 -0300

    Fix: fix structure modal form

--- src/js/components/modal/ConfirmPassFormCtrl.js
+++ src/js/components/modal/ConfirmPassFormCtrl.js
@@ -1,36 +1,36 @@
-import { FormCtrl } from "../../controllers/forms/FormCtrl.js";
+import { FormCtrl } from '../../controllers/FormCtrl.js';
 
 export class ConfirmPassFormCtrl extends FormCtrl {
   get _viewParams() {
     return {
-      id: "confirm-pass-form",
-      containerElement: document.querySelector(".modal__content"),
-      cssClass: "modal__form",
+      id: 'confirm-pass-form',
+      containerElement: document.querySelector('.modal__content'),
+      cssClass: 'modal__form',
     };
   }
 
-  get _inputParams() {
+  get _inputsConfig() {
     return [
       {
-        id: "transaction-password",
-        category: "password",
+        id: 'transaction-password',
+        category: 'password',
         strictToNumber: true,
-        ariaLabel: "Password",
+        ariaLabel: 'Password',
       },
     ];
   }
 
-  get _submitParams() {
+  get _submitConfig() {
     return {
-      id: "transaction-confirm",
-      cssClass: "modal-btn",
-      labelText: "Confirm",
+      id: 'transaction-confirm',
+      cssClass: 'modal-btn',
+      labelText: 'Confirm',
     };
   }
 
   // Add endpoint in the superclass constructor, when backend is defined
   // Each confirm pass modal has a different endpoint
   get _endpoint() {
-    return "";
+    return '';
   }
 }
