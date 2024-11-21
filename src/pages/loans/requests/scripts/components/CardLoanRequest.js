4ded8ac103b148eb3e4ba8cd8116f711759cf8b3
CleinerFG <cleinerfurlani@gmail.com>
2024-11-21 15:18:02 -0300

    Add feature: Custom cards for requests

--- src/pages/loans/requests/scripts/components/CardLoanRequest.js
+++ src/pages/loans/requests/scripts/components/CardLoanRequest.js
@@ -1,6 +1,6 @@
-import { CardComponent } from "../../../../../js/components/CardComponent.js";
-import { LoanRequestModel } from "../models/LoanRequestModel.js";
-import { capitalize } from "../../../../../js/utils/stringUtils.js";
+import { CardComponent } from '../../../../../js/components/CardComponent.js';
+import { LoanRequestModel } from '../models/LoanRequestModel.js';
+import { capitalize } from '../../../../../js/utils/stringUtils.js';
 
 /**
  * Represents a card component specifically for displaying Loan Request.
@@ -26,31 +26,48 @@ export class CardLoanRequest extends CardComponent {
   get _cardItemsTemplate() {
     return [
       {
-        label: this._model.creditor ? "Creditor" : "Debtor",
+        label: this._model.creditor ? 'Creditor' : 'Debtor',
         value: this._model.creditor || this._model.debtor,
       },
-      { label: "Date", value: this._model.date },
-      { label: "Value", value: this._model.value },
-      { label: "Description", value: this._model.description },
-      { label: "Installments", value: this._model.installments },
+      { label: 'Date', value: this._model.date },
+      { label: 'Value', value: this._model.value },
+      { label: 'Description', value: this._model.description },
+      { label: 'Installments', value: this._model.installments },
       {
-        label: "Installment Value",
+        label: 'Installment Value',
         value: this._model.installmentValue,
       },
-      { label: "Interest Rate", value: this._model.rate },
+      { label: 'Interest Rate', value: this._model.rate },
     ];
   }
 
   get _cardHeaderTemplate() {
-    return capitalize(this._model.status);
+    let cssClass;
+    if (this._model.status === 'accepted') {
+      cssClass = 'p__success';
+    } else if (this._model.status === 'denied') {
+      cssClass = 'p__fail';
+    }
+    const str = `<p class="${cssClass}">${capitalize(this._model.status)}</p>`;
+    return str;
   }
 
   get _cardFooterTemplate() {
-    return `
-     <button id="btn-loan-request-${this._model.id}" class="btn card-data__btn">
-        Action
-      </button>
+    const openedBtns = `
+     <button id="btn-loan-request-confirm-${this._model.id}" class="btn card-data__btn">
+        OK
+     </button>
     `;
+    const receivedBtns = `
+     <button id="btn-loan-request-approve-${this._model.id}" class="btn card-data__btn btn-success">
+        Approve 
+     </button>
+     <button id="btn-loan-request-repprove-${this._model.id}" class="btn card-data__btn btn-fail">
+        Repprove
+     </button>
+    `;
+    const btns = this._model.type === 'opened' ? openedBtns : receivedBtns;
+    return btns;
   }
 
   /**
