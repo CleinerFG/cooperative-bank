afadd2a513aa229f18aa39a950f2a27ef0af84bd
CleinerFG <cleinerfurlani@gmail.com>
2024-12-09 17:45:04 -0300

    SPA Test: create client routes

--- testing/client/static/js/index.js
+++ testing/client/static/js/index.js
@@ -1,1 +1,25 @@
-console.log("JS is loaded!");
+const router = async () => {
+  const routes = [
+    { path: '/', view: () => console.log('Viewing Dashboard') },
+    { path: '/posts', view: () => console.log('Viewing Posts') },
+    { path: '/settings', view: () => console.log('Viewing Settings') },
+  ];
+
+  const potentialMatches = routes.map((route) => {
+    return {
+      route: route,
+      isMatch: location.pathname === route.path,
+    };
+  });
+
+  let match = potentialMatches.find((potentialMatch) => potentialMatch.isMatch);
+  if (!match) {
+    match = {
+      route: routes[0],
+    };
+  }
+
+  match.route.view();
+};
+
+document.addEventListener('DOMContentLoaded', router);
