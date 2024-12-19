71c0ecb811256c9e0f3e660db7d24fe4bb08786c
CleinerFG <cleinerfurlani@gmail.com>
2024-12-19 17:26:29 -0300

    Refactor: ApiService - clean code

--- src/client/app/static/js/service/ApiService.js
+++ src/client/app/static/js/service/ApiService.js
@@ -1,28 +1,15 @@
 /**
- * A utility class that provides methods for interacting with
- * a remote API. It abstracts common HTTP methods (GET, POST, PUT, DELETE)
- * for interacting with a backend service.
- *
- * @class
+ * Provides methods for interacting with a remote API.
+ * It abstracts common HTTP methods (GET, POST, PUT, DELETE) for interacting with a backend service.
  */
 export class ApiService {
-  /**
-   * Base URL for API requests.
-   *
-   * @private
-   * @type {string}
-   * @constant
-   */
   static #BASE_URL = 'http://localhost:8080/';
 
   /**
    * Helper function to perform the fetch operation for various HTTP methods.
-   *
-   * @async
-   * @private
-   * @param {string} endpoint - The API endpoint to interact with.
-   * @param {object} [options={}] - The options to configure the fetch request.
-   * @returns {Promise<object>} The parsed JSON response.
+   * @param {string} endpoint
+   * @param {object} [options={}]
+   * @returns {Promise<object>}
    */
   static async #fetchData(endpoint, options = {}) {
     const res = await fetch(`${ApiService.#BASE_URL}${endpoint}`, options);
@@ -31,24 +18,18 @@ export class ApiService {
 
   /**
    * Fetches data from the specified endpoint using the GET method.
-   *
-   * @async
-   * @public
-   * @param {string} endpoint - The API endpoint to fetch data from.
-   * @returns {Promise<object>} The parsed JSON response from the API.
+   * @param {string} endpoint
+   * @returns {Promise<object>}
    */
   static async fetchFrom(endpoint) {
     return await ApiService.#fetchData(endpoint);
   }
 
   /**
-   * Sends data to the specified endpoint using the POST method.
-   *
-   * @async
-   * @public
-   * @param {string} endpoint - The API endpoint to send data to.
-   * @param {object} data - The data to send to the API.
-   * @returns {Promise<object>} The parsed JSON response from the API.
+   * Sends data using the POST method.
+   * @param {string} endpoint
+   * @param {object} data
+   * @returns {Promise<object>}
    */
   static async sendTo(endpoint, data) {
     return await ApiService.#fetchData(endpoint, {
@@ -61,13 +42,10 @@ export class ApiService {
   }
 
   /**
-   * Updates data at the specified endpoint using the PUT method.
-   *
-   * @async
-   * @public
-   * @param {string} endpoint - The API endpoint to update data at.
-   * @param {Object} data - The data to update at the API.
-   * @returns {Promise<object>} The parsed JSON response from the API.
+   * Updates data using the PUT method.
+   * @param {string} endpoint
+   * @param {Object} data
+   * @returns {Promise<object>}
    */
   static async updateTo(endpoint, data) {
     return await ApiService.#fetchData(endpoint, {
@@ -80,12 +58,9 @@ export class ApiService {
   }
 
   /**
-   * Deletes data from the specified endpoint using the DELETE method.
-   *
-   * @async
-   * @public
-   * @param {string} endpoint - The API endpoint to delete data from.
-   * @returns {Promise<object>} The parsed JSON response from the API.
+   * Deletes data using the DELETE method.
+   * @param {string} endpoint
+   * @returns {Promise<object>}
    */
   static async deleteFrom(endpoint) {
     return await ApiService.#fetchData(endpoint, {
