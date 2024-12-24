122f3e6749e3b3f10a5a87bcc8d3a0600bcefe0a
CleinerFG <cleinerfurlani@gmail.com>
2024-12-24 10:16:54 -0300

    Fix: base url apiService

--- src/client/app/static/js/service/ApiService.js
+++ src/client/app/static/js/service/ApiService.js
@@ -3,13 +3,11 @@
  * It abstracts common HTTP methods (GET, POST, PUT, DELETE) for interacting with a backend service.
  */
 export class ApiService {
-  static #BASE_URL = 'http://localhost:8080/';
+  static #BASE_URL = 'http://localhost:8080/app/';
 
   /**
-   * Helper function to perform the fetch operation for various HTTP methods.
    * @param {string} endpoint
    * @param {object} [options={}]
-   * @returns {Promise<object>}
    */
   static async #fetchData(endpoint, options = {}) {
     const res = await fetch(`${ApiService.#BASE_URL}${endpoint}`, options);
@@ -17,19 +15,15 @@ export class ApiService {
   }
 
   /**
-   * Fetches data from the specified endpoint using the GET method.
    * @param {string} endpoint
-   * @returns {Promise<object>}
    */
   static async fetchFrom(endpoint) {
     return await ApiService.#fetchData(endpoint);
   }
 
   /**
-   * Sends data using the POST method.
    * @param {string} endpoint
    * @param {object} data
-   * @returns {Promise<object>}
    */
   static async sendTo(endpoint, data) {
     return await ApiService.#fetchData(endpoint, {
@@ -42,10 +36,8 @@ export class ApiService {
   }
 
   /**
-   * Updates data using the PUT method.
    * @param {string} endpoint
    * @param {Object} data
-   * @returns {Promise<object>}
    */
   static async updateTo(endpoint, data) {
     return await ApiService.#fetchData(endpoint, {
@@ -58,9 +50,7 @@ export class ApiService {
   }
 
   /**
-   * Deletes data using the DELETE method.
    * @param {string} endpoint
-   * @returns {Promise<object>}
    */
   static async deleteFrom(endpoint) {
     return await ApiService.#fetchData(endpoint, {
