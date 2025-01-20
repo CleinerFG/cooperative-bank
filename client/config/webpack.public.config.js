8d619166f70c0e14a1679fc81dfd98cc6fc394e2
CleinerFG <cleinerfurlani@gmail.com>
2025-01-20 10:23:07 -0300

    Update: webpack config

---
+++ client/config/webpack.public.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, '../src/public/js/index.js'),
+
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'public/static/js/[name].[contenthash].js',
+    chunkFilename: 'public/static/js/[name].[contenthash].js',
+    publicPath: '/',
+    clean: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+    minimize: true,
+    usedExports: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../src/public/index.html'),
+      filename: 'public/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../src/public/css'),
+          to: path.resolve(__dirname, '../dist/public/static/css'),
+        },
+        {
+          from: path.resolve(__dirname, '../src/public/assets'),
+          to: path.resolve(__dirname, '../dist/public/static/assets'),
+        },
+      ],
+    }),
+  ],
+  resolve: {
+    extensions: ['.js'],
+  },
+};
