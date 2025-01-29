245d2f17d7600b82088fb75c095acc381898c07e
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-01-29 19:38:53 -0300

    Fix: SIMULATE_WAIT_SERVER constant - is necessary webpack rebuild

--- client/src/global/js/constants.js
+++ client/src/global/js/constants.js
@@ -3,5 +3,7 @@ export const API_BASE_URL = `http://${SERVER_IP}:8080/api`;
 
 /** --- Dev test --- */
 
-/** Time in seconds when making a request to the server API */
+// Time in seconds when making a request to the server API
+// WARNING: The constant may be cached by Webpack in build-dev mode.
+// If you need to modify it, you might have to rebuild manually.
 export const SIMULATE_WAIT_SERVER = 0;
