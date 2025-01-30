e85351093c20a3578d756c4369a38f24a740af70
CleinerFG <cleinerfurlani@gmail.com>
2025-01-30 15:16:13 -0300

    Feature: implemented profile

--- client/src/app/js/pages/common/myAccount/MyAccountPageView.js
+++ client/src/app/js/pages/common/myAccount/MyAccountPageView.js
@@ -1,10 +1,30 @@
+import('../../../../css/pages/my-account.css');
+
 import { PageView } from '../../../../../global/js/views/PageView.js';
 
 export default class MyAccountPageView extends PageView {
   get _template() {
     return `
-    <h1 class="section__h1">In development</h1>
-    <p class="info-text">This page is under development...</p>
+     <div class="cover-photo">
+      <div class="profile-header">
+        <img src="/app/static/assets/images/profile.webp" alt="Profile photo" class="profile-photo">
+        <h1 class="profile-name">Meg Thomas</h1>
+      </div>
+      </div>
+      <div class="profile-info">
+        <div class="info-item">
+          <span class="info-label">Date of birth</span>
+          <span class="info-value">15/06/2002</span>
+        </div>
+        <div class="info-item">
+          <span class="info-label">E-mail</span>
+          <span class="info-value">meg.thomas@gmail.com</span>
+        </div>
+        <div class="info-item">
+          <span class="info-label">Registration date</span>
+          <span class="info-value">20/01/2025</span>
+        </div>
+      </div>
     `;
   }
 
