65a7a8c2abbfb082ee52b44304474709f9e981de
CleinerFG <cleinerfurlani@gmail.com>
2025-02-06 17:43:54 -0300

    Refactor: card loan request

--- client/src/app/js/pages/loan/requests/components/CardLoanRequestOpened.js
+++ client/src/app/js/pages/loan/requests/components/CardLoanRequestOpened.js
@@ -1,18 +1,41 @@
-import { Card } from '../../../../components/Card.js';
-import { LoanRequestModel } from '../models/LoanRequestModel.js';
+import { CardActiveLoan } from '../../overview/components/CardActiveLoan.js';
 import { ConfirmPassModal } from '../../../../../js/components/modal/ConfirmPassModal.js';
 import { capitalize } from '../../../../../../global/js/utils/stringUtils.js';
+import {
+  numberToCurrency,
+  numberToPercent,
+} from '../../../../../../global/js/utils/formatters.js';
+import { handleIconDark } from '../../../../../../global/js/utils/themeUtils.js';
+import { ASSETS_ROUTE } from '../../../../constants/routes.js';
+
+/**
+ * @typedef {object} LoanRequestOpenedData
+ * @property {string} id
+ * @property {'personal'|'auto'|'mortgage'} modality
+ * @property {string} creditor
+ * @property {string} debtor
+ * @property {string} date
+ * @property {number} creditValue
+ * @property {number} totalAmount
+ * @property {number} installments
+ * @property {number} installmentValue
+ * @property {number} rate
+ * @property {'rejected'|'pending'} status
+ */
 
 /**
  * Represents a card component specifically for displaying Loan Request Opened.
  */
-export class CardLoanRequestOpened extends Card {
-  get _ModelClass() {
-    return LoanRequestModel;
+export class CardLoanRequestOpened extends CardActiveLoan {
+  /**
+   * @type {LoanRequestOpenedData}
+   */
+  get _apiData() {
+    return super._apiData;
   }
 
-  get _id() {
-    return `loan-request-${this._model.id}`;
+  get _cssId() {
+    return `loan-request-${this._index}`;
   }
 
   get _cssClass() {
@@ -21,32 +44,44 @@ export class CardLoanRequestOpened extends Card {
 
   get _itemsArray() {
     return [
-      { label: 'Creditor', value: this._model.creditor },
-      { label: 'Date', value: this._model.date },
-      { label: 'Value', value: this._model.value },
-      { label: 'Installments', value: this._model.installments },
       {
-        label: 'Installment Value',
-        value: this._model.installmentValue,
+        label: 'Creditor',
+        value: this._apiData.creditor,
       },
-      { label: 'Interest Rate', value: this._model.rate },
+      {
+        label: 'Credit value',
+        value: numberToCurrency(this._apiData.creditValue),
+      },
+      {
+        label: 'Total amount',
+        value: numberToCurrency(this._apiData.totalAmount),
+      },
+      {
+        label: 'Installment value',
+        value: numberToCurrency(this._apiData.installmentValue),
+      },
+      {
+        label: 'Interest rate',
+        value: `${numberToPercent(this._apiData.rate)} p.m.`,
+      },
+      {
+        label: 'Installments',
+        value: this._apiData.installments,
+      },
     ];
   }
 
   get _headerTemplate() {
-    let cssClass = 'span-pending';
-    switch (this._model.status) {
-      case 'accepted':
-        cssClass = 'span-success';
-        break;
-      case 'denied':
-        cssClass = 'span-fail';
-        break;
-      default:
-        break;
-    }
-    const str = `<span class="${cssClass}">${capitalize(this._model.status)}</span>`;
-    return str;
+    const modality = this._modalityMap[this._apiData.modality];
+    const cssClass =
+      this._apiData.status === 'pending' ? 'span-pending' : 'span-fail';
+    return `
+        <div class="loan-modality">
+          <img src="${modality.imgSrc}" alt="Modality" class="icon ${handleIconDark()}">
+          <span>${modality.desc}</span>
+        </div>
+        <span class="${cssClass}">${capitalize(this._apiData.status)}</span>
+        `;
   }
 
   get _footerTemplate() {
@@ -54,21 +89,21 @@ export class CardLoanRequestOpened extends Card {
   }
 
   get _buttonsByStatus() {
-    const action = this._model.status === 'pending' ? 'cancel' : 'confirm';
-    const cssClass = this._model.status === 'pending' ? 'btn-fail' : '';
+    const action = this._apiData.status === 'ending' ? 'Cancel' : 'Confirm';
+    const cssClass = this._apiData.status === 'pending' ? 'btn-fail' : '';
     return `
-      <button id="btn-${action}-${this._model.id}" class="btn card-data__btn ${cssClass}">
-         ${capitalize(action)}
+      <button id="btn-${action}-${this._index}" class="btn card-data__btn ${cssClass}">
+         ${action}
       </button>
      `;
   }
 
   _handleModal() {
-    const action = this._model.status === 'pending' ? 'cancel' : 'confirm';
-    document
-      .querySelector(`#btn-${action}-${this._model.id}`)
-      .addEventListener('click', () => {
-        new ConfirmPassModal();
-      });
+    // const action = this._apiData.status === 'pending' ? 'cancel' : 'confirm';
+    // document
+    //   .querySelector(`#btn-${action}-${this._index}`)
+    //   .addEventListener('click', () => {
+    //     new ConfirmPassModal();
+    //   });
   }
 }
