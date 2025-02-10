0b46ad4bf900cd73773a6b1be23fe73cc18fca32
CleinerFG <cleinerfurlani@gmail.com>
2025-02-10 15:01:12 -0300

    Feature: loan service

---
+++ client/src/app/js/services/LoanService.js
@@ -0,0 +1,21 @@
+import { API_BASE_URL } from '../../../global/js/constants.js';
+
+class LoanService {
+  /**
+   * @param {'payable'|'receivable'} category
+   */
+  async getActiveLoans(category) {
+    const res = await fetch(`${API_BASE_URL}/loan/overview/${category}`);
+    return await res.json();
+  }
+
+  /**
+   * @param {'received'|'opened'} category
+   */
+  async getLoanRequests(category) {
+    const res = await fetch(`${API_BASE_URL}/loan/overview/${category}`);
+    return await res.json();
+  }
+}
+
+export default new LoanService();
