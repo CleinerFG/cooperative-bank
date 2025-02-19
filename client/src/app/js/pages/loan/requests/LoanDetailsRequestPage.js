700a8c088b8a0881d66e06f31b3b2e790d226de2
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-02-19 20:01:01 -0300

    Fix: status in the opened requests

--- client/src/app/js/pages/loan/requests/LoanDetailsRequestPage.js
+++ client/src/app/js/pages/loan/requests/LoanDetailsRequestPage.js
@@ -1,10 +1,22 @@
 import LoanDetailsPage from '../LoanDetailsPage.js';
 import loanService from '../../../services/LoanService.js';
+import { capitalize } from '../../../../../global/js/utils/stringUtils.js';
 
 export default class LoanDetailsRequestPage extends LoanDetailsPage {
   _fetchService(id) {
-      return loanService.getLoanDetailsRequest(id);
-    }
+    return loanService.getLoanDetailsRequest(id);
+  }
 
-  _setCustomConfig() {}
+  _customInfoDataHandler() {
+    if (this._queryParams.category === 'received') return;
+    this._addInfoItemData(0, {
+      label: 'status',
+      getValue: () => capitalize(this._apiData.status),
+      img: 'icon-status.svg',
+    });
+  }
+
+  _setCustomConfig() {
+    this._customInfoDataHandler();
+  }
 }
