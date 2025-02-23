e6017e783c903a2802229fdb904cd790c2177616
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-02-23 11:03:10 -0300

    Fix: bug icon dark in form

--- client/src/global/js/components/FormView.js
+++ client/src/global/js/components/FormView.js
@@ -1,6 +1,7 @@
 import '../types/formElementsType.js';
 import { SubmitButton } from './form-elements/SubmitButton.js';
 import { AssetManager } from '../core/AssetManager.js';
+import { handleIconDark } from '../utils/themeUtils.js';
 
 export class FormView {
   #containerElement;
@@ -53,7 +54,7 @@ export class FormView {
       const icon = `${AssetManager.iconsPath}${this.#header.icon}`;
       return `
       <div class="form-header">
-        <img class="icon form-icon" src="${icon}" alt="Icon ${this.#id}">
+        <img class="icon form-icon ${handleIconDark()}" src="${icon}" alt="Icon ${this.#id}">
         <h3 class="form-title">${this.#header.title}</h3>
       </div>
       `;
