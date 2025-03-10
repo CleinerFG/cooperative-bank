900a2fc0e3f8d960998f456fbc1283a3346ad907
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-03-10 11:58:41 -0300

    Feature: loan controller

---
+++ server/src/controllers/loanController.js
@@ -0,0 +1,23 @@
+const service = require('../services/loanService');
+
+module.exports = {
+  async getAllByCategory(req, res) {
+    const category = req.params.category;
+    try {
+      const requests = await service.getAllByCategory(category);
+      return res.json(requests);
+    } catch (e) {
+      return res.status(400).json({ error: e.message });
+    }
+  },
+  async getDetailsByCategoryAndId(req, res) {
+    const category = req.params.category;
+    const id = req.params.id;
+    try {
+      const request = await service.getDetailsByCategoryAndId(category, id);
+      return res.json(request);
+    } catch (e) {
+      return res.status(400).json({ error: e.message });
+    }
+  },
+};
