d0293bb49a526c970f04917bbd6659e892d925d2
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-03-10 11:55:58 -0300

    Feature: loan service

---
+++ server/src/services/loanService.js
@@ -0,0 +1,22 @@
+const repository = require('../repositories/loanRepository');
+const { isString } = require('../lib/utils/validators');
+const { loanCategoryIsValid } = require('../lib/helpers/loanValidators');
+
+module.exports = {
+  /**
+   * @param {'payable'|'receivable'} category
+   */
+  async getAllByCategory(category) {
+    loanCategoryIsValid(category);
+    return repository.getAllByCategory(category);
+  },
+  /**
+   * @param {'payable'|'receivable'} category
+   * @param {string} id
+   */
+  async getDetailsByCategoryAndId(category, id) {
+    loanCategoryIsValid(category);
+    isString(id);
+    return repository.getDetailsByCategoryAndId(category, id);
+  },
+};
