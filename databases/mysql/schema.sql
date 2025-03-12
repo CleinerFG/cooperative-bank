d0545ea63a678998a2e048b33755f105f4f8275c
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-03-12 11:35:31 -0300

    Feature: Base mysql schema

--- databases/mysql/schema.sql
+++ databases/mysql/schema.sql
@@ -1,14 +1,135 @@
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  cpf BIGINT NOT NULL UNIQUE,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL UNIQUE
+  full_name VARCHAR(255) NOT NULL,
+  cpf VARCHAR(11) UNIQUE NOT NULL,
+  birth DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS account_statuses (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  description enum('active', 'closed') NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS accounts (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  user_id INT,
-  balance DECIMAL(10, 2) DEFAULT 0.00,
-  registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  user_id INT NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(60) NOT NULL,
+  created_at DATETIME DEFAULT (now()),
+  operation_password VARCHAR(60) DEFAULT NULL,
+  balance DECIMAL(14,2) DEFAULT 0,
+  status_id INT DEFAULT 1,
+
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (status_id) REFERENCES account_statuses (id)
 );
+
+CREATE TABLE IF NOT EXISTS loan_statuses (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  description enum('pending', 'active', 'completed', 'canceled', 'rejected') NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS loans (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  debtor_account_id INT NOT NULL,
+  creditor_account_id INT NOT NULL,
+  contract_date DATETIME NOT NULL,
+  value DECIMAL(11,2) NOT NULL,
+  month_rate DECIMAL(4,2) NOT NULL,
+  installments_qty INT NOT NULL,
+  status_id INT DEFAULT 1,
+
+  FOREIGN KEY (debtor_account_id) REFERENCES accounts (id),
+  FOREIGN KEY (creditor_account_id) REFERENCES accounts (id),
+  FOREIGN KEY (status_id) REFERENCES loan_statuses (id)
+);
+
+CREATE TABLE IF NOT EXISTS loan_installment_statuses (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  description enum('pending', 'paid') NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS loan_installments (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  loan_id INT NOT NULL,
+  value DECIMAL(11,2) NOT NULL,
+  due_date date NOT NULL,
+  status_id INT DEFAULT 1,
+
+  FOREIGN KEY (loan_id) REFERENCES loans (id),
+  FOREIGN KEY (status_id) REFERENCES loan_installment_statuses (id)
+);
+
+CREATE TABLE IF NOT EXISTS loan_payments (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  installment_id INT NOT NULL,
+  date DATETIME DEFAULT (now()),
+  value DECIMAL(11,2) NOT NULL,
+
+  FOREIGN KEY (installment_id) REFERENCES loan_installments (id)
+);
+
+CREATE TABLE IF NOT EXISTS transfer_statuses (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  description enum('sent', 'refunded') NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS transfers (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  sender_account_id INT NOT NULL,
+  receiver_account_id INT NOT NULL,
+  date DATETIME DEFAULT (now()),
+  value DECIMAL(14,2) NOT NULL,
+  status_id INT DEFAULT 1,
+
+  FOREIGN KEY (sender_account_id) REFERENCES accounts (id),
+  FOREIGN KEY (receiver_account_id) REFERENCES accounts (id),
+  FOREIGN KEY (status_id) REFERENCES transfer_statuses (id)
+);
+
+CREATE TABLE IF NOT EXISTS investment_statuses (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  description enum('active', 'liquidated', 'canceled') NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS investments (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  account_id INT NOT NULL,
+  contract_date DATETIME NOT NULL,
+  liquidy_date DATETIME NOT NULL,
+  month_rate DECIMAL(4,2) NOT NULL,
+  value DECIMAL(11,2) NOT NULL,
+  status_id INT DEFAULT 1,
+
+  FOREIGN KEY (account_id) REFERENCES accounts (id),
+  FOREIGN KEY (status_id) REFERENCES investment_statuses (id)
+);
+
+CREATE TABLE IF NOT EXISTS operation_categories (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  record_table enum('loans', 'loan_payments', 'investments', 'transfers') NOT NULL
+)
+COMMENT = 'This table defines the categories of operations, specifying the type of record
+associated with each operation. The "record_table" column represents the source table
+of the record, which is dynamically determined based on the "category_id"
+in the related "operations" table. This enables a polymorphic
+relationship to different tables depending on the operations category.
+';
+
+CREATE TABLE IF NOT EXISTS operation_flows (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  description enum('credit', 'debit') NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS operations (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  account_id INT NOT NULL,
+  category_id INT NOT NULL,
+  category_record_id INT NOT NULL COMMENT 'Polymorphic relationship: contains a record from the table specified in "record_table" of the table "operation_categories" according to the "category_id" of this table',
+  flow_id INT NOT NULL,
+  value DECIMAL(14,2) NOT NULL,
+
+  FOREIGN KEY (account_id) REFERENCES accounts (id),
+  FOREIGN KEY (category_id) REFERENCES operation_categories (id),
+  FOREIGN KEY (flow_id) REFERENCES operation_flows (id)
+  );
