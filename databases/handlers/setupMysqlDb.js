b294781c956ecb5b047190795ddf19432bb76988
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-03-12 13:53:33 -0300

    Refactor: setup mysql

--- databases/handlers/setupMysqlDb.js
+++ databases/handlers/setupMysqlDb.js
@@ -4,18 +4,15 @@ const {
   MYSQL_DB_NAME_PROD,
 } = require('../constants.js');
 
-const displayErrors = require('../utils/displayErrors.js');
-const createMysqlDb = require('../mysql/scripts/createDb.js');
 const dbName = ENV === 'development' ? MYSQL_DB_NAME_DEV : MYSQL_DB_NAME_PROD;
+const createDb = require('../mysql/scripts/createDb.js');
+const createTables = require('../mysql/scripts/createTables.js');
 
 module.exports = async () => {
-  try {
-    console.log('---> [DB Setup - Mysql]');
-    await createMysqlDb(dbName);
-  } catch (e) {
-    console.error('Error creating Mysql Database:');
-    displayErrors(e);
-  } finally {
-    console.log('---> [DB Setup - Mysql] Finished');
+  console.log('---> [DB Setup - Mysql]');
+  const dbCreated = await createDb(dbName);
+  if (dbCreated) {
+    const tablesCreated = await createTables(dbName);
   }
+  console.log('---> [DB Setup - Mysql] Finished');
 };
