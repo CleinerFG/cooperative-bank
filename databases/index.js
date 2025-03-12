fcd71f272fe95d1474e355b32bb1636a72cef368
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-03-12 17:05:34 -0300

    Feature: log separator set dbs

--- databases/index.js
+++ databases/index.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 
 const setupMysqlDb = require('./handlers/setupMysqlDb');
 const setupMongoDb = require('./handlers/setupMongoDb');
+const log = require('./utils/consoleLogger');
 
 (async () => {
   await setupMysqlDb();
+  log.info(
+    '~ - ~ - ~ - ~ - ~ - ~ - ~ - ~ - ~ - ~ - ~ - ~ - ~ - ~ - ~ - ~ - ~'
+  );
   await setupMongoDb();
 })();
