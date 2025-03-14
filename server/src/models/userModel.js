3376523e6905a137ff4d473b0f4d3b555a284969
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-03-14 16:49:51 -0300

    Update: account model in user model

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../../config/mysqlConfig');
 
-module.exports = sequelize.define(
+const userModel = (module.exports = sequelize.define(
   'user',
   {
     id: {
@@ -27,8 +27,34 @@ module.exports = sequelize.define(
       type: DataTypes.DATEONLY,
       allowNull: false,
     },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING(60),
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    operationPassword: {
+      type: DataTypes.STRING(60),
+      allowNull: true,
+      defaultValue: null,
+    },
+    balance: {
+      type: DataTypes.DECIMAL(14, 2),
+      allowNull: false,
+      defaultValue: 0,
+    },
   },
   {
     tableName: 'users',
   }
-);
+));
+
+module.exports = userModel;
