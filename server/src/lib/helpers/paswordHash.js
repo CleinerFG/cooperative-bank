6dae87e13fbe70c88c5869c91003270668b0aba5
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-03-14 12:52:01 -0300

    Feature: password hash types

--- server/src/lib/helpers/paswordHash.js
+++ server/src/lib/helpers/paswordHash.js
@@ -4,6 +4,9 @@ const {
   ComparePasswordError,
 } = require('../../errors/HashingError');
 
+/**
+ * @param {string} password
+ */
 const createPasswordHash = async (password) => {
   try {
     const salt = await bcrypt.genSalt(10);
@@ -14,6 +17,10 @@ const createPasswordHash = async (password) => {
   }
 };
 
+/**
+ * @param {string} password
+ * @param {string} hashedPassword
+ */
 const comparePassword = async (password, hashedPassword) => {
   try {
     const match = await bcrypt.compare(password, hashedPassword);
