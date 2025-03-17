64f78209ef5736721e7c6aae0c67fab4d4604103
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-03-17 09:51:07 -0300

    Update: model User

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,60 +1,47 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../../config/mysqlConfig');
+const sequelize = require('../../config/databases/mysqlConfig');
 
-const userModel = (module.exports = sequelize.define(
-  'user',
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    opaqueId: {
-      type: DataTypes.STRING(36),
-      unique: true,
-      allowNull: false,
-    },
-    fullName: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-    },
-    cpf: {
-      type: DataTypes.STRING(11),
-      unique: true,
-      allowNull: false,
-    },
-    birth: {
-      type: DataTypes.DATEONLY,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING(60),
-      allowNull: false,
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    operationPassword: {
-      type: DataTypes.STRING(60),
-      allowNull: true,
-      defaultValue: null,
-    },
-    balance: {
-      type: DataTypes.DECIMAL(14, 2),
-      allowNull: false,
-      defaultValue: 0,
-    },
+const UserModel = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    tableName: 'users',
-  }
-));
+  opaqueId: {
+    type: DataTypes.STRING(36),
+    unique: true,
+    allowNull: false,
+  },
+  fullName: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  cpf: {
+    type: DataTypes.STRING(11),
+    unique: true,
+    allowNull: false,
+  },
+  birth: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING(60),
+    allowNull: false,
+  },
+  operationPassword: {
+    type: DataTypes.STRING(60),
+  },
+  balance: {
+    type: DataTypes.DECIMAL(14, 2),
+    allowNull: false,
+    defaultValue: 0,
+  },
+});
 
-module.exports = userModel;
+module.exports = UserModel;
