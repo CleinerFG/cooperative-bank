427fd1fb46b9d0c6cf502623551e5bbabc898b46
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-03-17 14:56:51 -0300

    Fix: get db name

--- server/src/config/databases/mysql/scripts/syncModels.js
+++ server/src/config/databases/mysql/scripts/syncModels.js
@@ -4,7 +4,7 @@ const { log, logRow } = require('../../../../lib/utils/consoleLogger');
 
 module.exports = async () => {
   log('section', 'Syncing models...');
-  await sequelize.sync({ force: true, match: /_(testmode|devmode)$/ });
+  await sequelize.sync({ force: true, match: /_(test|dev)$/ });
   log('content', 'All models were synchronized successfully');
   logRow('section');
 };
