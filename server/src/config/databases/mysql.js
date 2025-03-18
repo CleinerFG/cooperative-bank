35543bb45ad3f6aed64145188cb5b18c1c1223c3
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-03-18 18:19:54 -0300

    Feature: sequelize connection

---
+++ server/src/config/databases/mysql.js
@@ -0,0 +1,29 @@
+const {
+  MYSQL_HOST,
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  MYSQL_PORT,
+  MYSQL_DB_NAME_BY_ENV,
+} = require('../constants');
+
+module.exports = {
+  dialect: 'mysql',
+  host: MYSQL_HOST,
+  username: MYSQL_USER,
+  password: MYSQL_PASSWORD,
+  database: MYSQL_DB_NAME_BY_ENV,
+  port: MYSQL_PORT,
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  define: {
+    underscored: true,
+  },
+};
