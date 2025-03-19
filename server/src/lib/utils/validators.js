c8fcf9d82fb48d4990932b7dc3da1392729ee116
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-03-19 20:06:29 -0300

    Refactor: validators improvement errors

--- server/src/lib/utils/validators.js
+++ server/src/lib/utils/validators.js
@@ -1,13 +1,10 @@
-const StringError = require('../../errors/StringError');
-
-const isString = (value) => {
-  if (typeof value !== 'string') {
-    throw new StringError('id');
-  }
+const datetimeValidator = (dateString) => {
+  const date = new Date(dateString);
+  if (isNaN(date.getTime())) throw new Error('invalidDatetime');
   return true;
 };
 
-const emailValidator = (value) => {
+const emailValidator = (string) => {
   const localPart = /^[\w._%+-]+/;
   const domainName = /[\w.-]+/;
   const topLevelDomain = /\.[\w]{2,}$/;
@@ -15,16 +12,16 @@ const emailValidator = (value) => {
   const regex = new RegExp(
     `^${localPart.source}@${domainName.source}${topLevelDomain.source}$`
   );
-  if (!regex.test(value)) return false;
+  if (!regex.test(string)) throw new Error('invalidEmail');
   return true;
 };
 
 const cpfValidator = (value) => {
   value = value.replace(/[.-]/g, '');
 
-  if (!/^[0-9]{11}$/.test(value)) return false;
+  if (!/^[0-9]{11}$/.test(value)) throw new Error('invalidCpf');
 
-  if (/^(\d)\1{10}$/.test(value)) return false;
+  if (/^(\d)\1{10}$/.test(value)) throw new Error('invalidCpf');
 
   let sum = 0,
     remainder;
@@ -32,15 +29,15 @@ const cpfValidator = (value) => {
   for (let i = 1; i <= 9; i++) sum += parseInt(value[i - 1]) * (11 - i);
   remainder = (sum * 10) % 11;
   if (remainder === 10 || remainder === 11) remainder = 0;
-  if (remainder !== parseInt(value[9])) return false;
+  if (remainder !== parseInt(value[9])) throw new Error('invalidCpf');
 
   sum = 0;
   for (let i = 1; i <= 10; i++) sum += parseInt(value[i - 1]) * (12 - i);
   remainder = (sum * 10) % 11;
   if (remainder === 10 || remainder === 11) remainder = 0;
-  if (remainder !== parseInt(value[10])) return false;
+  if (remainder !== parseInt(value[10])) throw new Error('invalidCpf');
 
   return true;
 };
 
-module.exports = { isString, cpfValidator, emailValidator };
+module.exports = { datetimeValidator, cpfValidator, emailValidator };
