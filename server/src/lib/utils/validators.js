e6896e062f7efe3a86f4852f6a49519c3eca2bff
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-03-19 14:57:57 -0300

    Feature: add email validator

--- server/src/lib/utils/validators.js
+++ server/src/lib/utils/validators.js
@@ -1,9 +1,5 @@
 const StringError = require('../../errors/StringError');
-const InvalidCpfError = require('../../errors/InvalidCpfError');
 
-/**
- * @param {string} value
- */
 const isString = (value) => {
   if (typeof value !== 'string') {
     throw new StringError('id');
@@ -11,15 +7,24 @@ const isString = (value) => {
   return true;
 };
 
-/**
- * @param {string} value
- */
+const emailValidator = (value) => {
+  const localPart = /^[\w._%+-]+/;
+  const domainName = /[\w.-]+/;
+  const topLevelDomain = /\.[\w]{2,}$/;
+
+  const regex = new RegExp(
+    `^${localPart.source}@${domainName.source}${topLevelDomain.source}$`
+  );
+  if (!regex.test(value)) return false;
+  return true;
+};
+
 const cpfValidator = (value) => {
   value = value.replace(/[.-]/g, '');
 
-  if (!/^[0-9]{11}$/.test(value)) throw new InvalidCpfError();
+  if (!/^[0-9]{11}$/.test(value)) return false;
 
-  if (/^(\d)\1{10}$/.test(value)) throw new InvalidCpfError();
+  if (/^(\d)\1{10}$/.test(value)) return false;
 
   let sum = 0,
     remainder;
@@ -27,15 +32,15 @@ const cpfValidator = (value) => {
   for (let i = 1; i <= 9; i++) sum += parseInt(value[i - 1]) * (11 - i);
   remainder = (sum * 10) % 11;
   if (remainder === 10 || remainder === 11) remainder = 0;
-  if (remainder !== parseInt(value[9])) throw new InvalidCpfError();
+  if (remainder !== parseInt(value[9])) return false;
 
   sum = 0;
   for (let i = 1; i <= 10; i++) sum += parseInt(value[i - 1]) * (12 - i);
   remainder = (sum * 10) % 11;
   if (remainder === 10 || remainder === 11) remainder = 0;
-  if (remainder !== parseInt(value[10])) throw new InvalidCpfError();
+  if (remainder !== parseInt(value[10])) return false;
 
   return true;
 };
 
-module.exports = { isString, cpfValidator };
+module.exports = { isString, cpfValidator, emailValidator };
