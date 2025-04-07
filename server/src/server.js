523c172852e3b38c4bbe8fb7b0d581a4f950468d
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-04-07 12:06:58 -0300

    Refactor: routes

--- server/src/server.js
+++ server/src/server.js
@@ -4,13 +4,13 @@ require('express-async-errors');
 const { host, port } = require('./config/config.js');
 const { APP_STATIC_DIR, PUBLIC_STATIC_DIR } = require('./config/constants.js');
 
+const express = require('express');
 const cookierParser = require('cookie-parser');
 
-const authTokenMiddleware = require('./middlewares/authTokenMiddleware.js');
 const logMiddleware = require('./middlewares/logMiddleware.js');
 const simulateDelayMiddleware = require('./middlewares/simulateDelayMiddleware.js');
-
-const express = require('express');
+const errorsMiddleware = require('./middlewares/errorsMiddleware.js');
+const routes = require('./routes/index.js');
 
 const app = express();
 
@@ -21,28 +21,10 @@ app.use('/app/static', express.static(APP_STATIC_DIR));
 app.use('/public/static', express.static(PUBLIC_STATIC_DIR));
 
 app.use(logMiddleware);
-
 app.use('/api', simulateDelayMiddleware);
 
-// New routes structure
-const authRoutes = require('./routes/authRoutes.js');
-const accountRoutes = require('./routes/accountRoutes.js');
-const userRoutes = require('./routes/userRoutes.js');
-const loanRoutes = require('./routes/loanRoutes.js');
-// const loanRequestsRoutes = require('./routes/loanRequestRoutes.js');
-// const loanInstallmentsRoutes = require('./routes/loanInstallmentsRoutes.js');
-// const notificationsRoutes = require('./routes/notificationsRoutes.js');
-const spaRoutes = require('./routes/spaRoutes.js');
-const errorsMiddleware = require('./middlewares/errorsMiddleware.js');
+routes(app);
 
-app.use('/api/auth', authRoutes);
-app.use('/api/account', accountRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/loans', loanRoutes);
-// app.use('/api/loans/requests', loanRequestsRoutes);
-// app.use('/api/loans/installments', loanInstallmentsRoutes);
-// app.use('/api/notifications', notificationsRoutes);
-app.use(spaRoutes);
 app.use(errorsMiddleware);
 
 app.listen(port, host, () => {
