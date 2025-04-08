e172731a2bd0baf4c4e31c35778e03c362c595c8
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-04-08 10:06:35 -0300

    Fix: catch error in middleware

--- server/src/middlewares/errorsMiddleware.js
+++ server/src/middlewares/errorsMiddleware.js
@@ -1,9 +1,13 @@
+const BaseError = require('../errors/BaseError');
 const InvalidJsonError = require('../errors/InvalidJsonError');
 const { serverErrorsLogger } = require('../lib/utils/loggers');
 
 module.exports = (err, req, res, next) => {
   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
     return new InvalidJsonError().sendResponse(res);
+  }
+  if (err instanceof BaseError) {
+    return err.sendResponse(res);
   }
   serverErrorsLogger.error(err);
   return new BaseError().sendResponse(res);
