9dc97c2fe6c73ec089238d24f6f62a372d479a8d
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-04-08 10:24:04 -0300

    Feature: class implementation in spa

--- server/src/controllers/spaController.js
+++ server/src/controllers/spaController.js
@@ -1,14 +1,20 @@
 const { APP_DIR, PUBLIC_DIR } = require('../config/constants');
 const path = require('path');
 
-module.exports = {
-  sendAppHtml: (req, res) => {
-    const filePath = path.join(APP_DIR, 'index.html');
-    res.sendFile(filePath);
-  },
+class SpaController {
+  async sendHtml(req, res, dir) {
+    const filePath = path.join(dir, 'index.html');
+    return res.sendFile(filePath);
+  }
 
-  sendPublicHtml: (req, res) => {
-    const filePath = path.join(PUBLIC_DIR, 'index.html');
-    res.sendFile(filePath);
-  },
-};
+  async sendAppHtml(req, res) {
+    return await this.sendHtml(req, res, APP_DIR);
+  }
+
+  async sendPublicHtml(req, res) {
+    return await this.sendHtml(req, res, PUBLIC_DIR);
+  }
+}
+
+const spaController = new SpaController();
+module.exports = spaController;
--- server/src/routes/spaRoutes.js
+++ server/src/routes/spaRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/spaController');
 
-router.get('/app*', controller.sendAppHtml);
-router.get(['/', '/login', '/register'], controller.sendPublicHtml);
+router.get('/app*', controller.sendAppHtml.bind(controller));
+router.get(
+  ['/', '/login', '/register'],
+  controller.sendPublicHtml.bind(controller)
+);
 
 module.exports = router;
