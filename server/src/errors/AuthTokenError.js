8a87b2e7c3dbb142e64c07489d830088a868b3d4
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-04-09 12:23:16 -0300

    Fix: status 401 in auth error

--- server/src/errors/AuthTokenError.js
+++ server/src/errors/AuthTokenError.js
@@ -2,7 +2,7 @@ const BaseError = require('./BaseError');
 
 class AuthTokenError extends BaseError {
   constructor(message) {
-    super({ error: 'token', message }, 400);
+    super({ error: 'token', message }, 401);
   }
 }
 
