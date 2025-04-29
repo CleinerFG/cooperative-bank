1287ab783a8ed3fefb0243fbb3e7be681aebe4fe
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-04-29 16:42:55 -0300

    Fix imports

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import Spa from './Spa.jsx';
 
 createRoot(document.getElementById('root')).render(
