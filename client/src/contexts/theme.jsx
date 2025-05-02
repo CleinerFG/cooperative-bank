2edea72971a338cb8ca6ecffcd99b36dd577bc5f
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-05-02 12:15:35 -0300

    Feature: theme provider

---
+++ client/src/contexts/theme.jsx
@@ -0,0 +1,39 @@
+import {
+  createContext,
+  useState,
+  useContext,
+  useEffect,
+  useCallback,
+} from 'react';
+
+const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  console.log('test');
+  const [theme, setTheme] = useState(() => {
+    const localTheme = localStorage.getItem('cooperative-bank-theme');
+    if (localTheme) return localTheme;
+
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light';
+  });
+
+  const toggleTheme = useCallback(() => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('cooperative-bank-theme', theme);
+    document.body.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useTheme = () => useContext(ThemeContext);
