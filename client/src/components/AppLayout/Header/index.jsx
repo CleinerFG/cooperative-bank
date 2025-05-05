f1879c3f1f0d470fd188d4f431167b77dcbe8e80
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-05-05 10:07:40 -0300

    Refactor: remove old header

--- client/src/components/AppLayout/Header/index.jsx
+++ client/src/components/AppLayout/Header/index.jsx
@@ -1,25 +1,10 @@
-import HomeLink from './HomeLink';
-import Menu from './Menu';
-
 import styles from '../../../styles/layout/header.module.scss';
 
 export default function Header() {
   return (
     <>
-      <header id="header" className={styles.header}>
-        <HomeLink />
-        {/*
-        <button className="btn-unset btn-notifications" type="button">
-          <div id="notifications-unread" className="counter"></div>
-          <img
-            className="icon"
-            src="/app/static/assets/icons/icon-notifications.svg"
-            alt="Notifications icon"
-          />
-        </button>
+      <header className={styles.header}>
 
-         */}
-        <Menu />
       </header>
     </>
   );
