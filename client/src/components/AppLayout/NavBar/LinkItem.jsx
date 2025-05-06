2d82c9f00245f3537df75eb8167818818a58b362
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-05-06 17:09:39 -0300

    Rename rediretTo to navigateTo in NavLinks

--- client/src/components/AppLayout/NavBar/LinkItem.jsx
+++ client/src/components/AppLayout/NavBar/LinkItem.jsx
@@ -3,12 +3,12 @@ import { NavLink } from 'react-router-dom';
 import * as Icons from 'lucide-react';
 import styles from './styles.module.scss';
 
-function LinkItem({ label, redirectTo, iconName }) {
+function LinkItem({ label, navigateTo, iconName }) {
   const Icon = Icons[iconName];
 
   return (
     <NavLink
-      to={redirectTo}
+      to={navigateTo}
       end
       className={({ isActive }) =>
         isActive ? styles.linkItemActive : styles.linkItem
--- client/src/components/AppLayout/NavBar/index.jsx
+++ client/src/components/AppLayout/NavBar/index.jsx
@@ -3,25 +3,25 @@ import ThemeButton from './ThemeButton';
 import styles from './styles.module.scss';
 
 const navLinks = [
-  { label: 'Home', redirectTo: '/app', iconName: 'House' },
+  { label: 'Home', navigateTo: '/app', iconName: 'House' },
   {
     label: 'Transactions',
-    redirectTo: '/app/transactions',
+    navigateTo: '/app/transactions',
     iconName: 'ArrowLeftRight',
   },
-  { label: 'Account', redirectTo: '/app/account', iconName: 'CircleUser' },
-  { label: 'Settings', redirectTo: '/app/settings', iconName: 'Settings' },
+  { label: 'Account', navigateTo: '/app/account', iconName: 'CircleUser' },
+  { label: 'Settings', navigateTo: '/app/settings', iconName: 'Settings' },
 ];
 
 export default function BottomNav() {
   return (
     <nav className={styles.container}>
-      {navLinks.map(({ label, redirectTo, iconName }) => (
+      {navLinks.map(({ label, navigateTo, iconName }) => (
         <LinkItem
           label={label}
-          redirectTo={redirectTo}
+          navigateTo={navigateTo}
           iconName={iconName}
-          key={redirectTo}
+          key={navigateTo}
         />
       ))}
       {/* <ThemeButton /> */}
