85490dddc2d2129b411d15bd6ef75b34ae21b585
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-05-11 11:57:08 -0300

    Remove: unused fragment

--- client/src/components/AppLayout/MotionWrapper.jsx
+++ client/src/components/AppLayout/MotionWrapper.jsx
@@ -6,17 +6,15 @@ function MotionWrapper({ motionKey, direction, children }) {
   const offset = 100;
   const xInital = direction === 'forward' ? offset : -offset;
   return (
-    <>
-      <motion.div
-        key={motionKey}
-        initial={{ x: `${xInital}%`, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ duration: 0.3, ease: 'easeOut' }}
-        className={styles.motionPage}
-      >
-        {children}
-      </motion.div>
-    </>
+    <motion.div
+      key={motionKey}
+      initial={{ x: `${xInital}%`, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ duration: 0.3, ease: 'easeOut' }}
+      className={styles.motionPage}
+    >
+      {children}
+    </motion.div>
   );
 }
 
