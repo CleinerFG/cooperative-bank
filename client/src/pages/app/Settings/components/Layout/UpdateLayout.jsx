358ae0b4fc62ce83eba8f1356663852114b9c421
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-05-12 09:48:01 -0300

    Fix: update settings alyout with infinite loop

--- client/src/pages/app/Settings/components/Layout/UpdateLayout.jsx
+++ client/src/pages/app/Settings/components/Layout/UpdateLayout.jsx
@@ -5,7 +5,8 @@ function UpdateLayout({ title, Icon }) {
   const { setLayoutProps } = useLayout();
   useEffect(() => {
     setLayoutProps({ title, Icon });
-  });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return null;
 }
 
