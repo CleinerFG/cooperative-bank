a42adfd86a9a6f70d8272f44240d3b8438cd927e
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-05-12 12:42:04 -0300

    Feat: settings layout identify sub route and add styles

--- client/src/pages/app/Settings/components/Layout/index.jsx
+++ client/src/pages/app/Settings/components/Layout/index.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useMatch } from 'react-router-dom';
 import { useState } from 'react';
 import { LayoutContext } from '../../contexts/layout';
 
@@ -6,9 +6,15 @@ import styles from './styles.module.scss';
 
 function SettingsLayout() {
   const [layoutProps, setLayoutProps] = useState({ title: '', Icon: null });
+  const matchRootRoute = useMatch('app/settings/');
+
   return (
     <LayoutContext.Provider value={{ layoutProps, setLayoutProps }}>
-      <div className={styles.titleContainer}>
+      <div
+        className={
+          matchRootRoute ? styles.titleContainer : styles.titleContainerWeak
+        }
+      >
         {layoutProps.Icon && <layoutProps.Icon className={styles.icon} />}
         <h1 className={styles.title}>{layoutProps.title}</h1>
       </div>
