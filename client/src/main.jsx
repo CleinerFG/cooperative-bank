5159e85f0ab414de575cc5092f8718f32717a5d2
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-05-16 17:54:23 -0300

    Fix: remove old theme provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,16 +4,13 @@ import { RouterProvider } from 'react-router-dom';
 
 import router from './router';
 import './i18n';
-import { ThemeProvider } from './contexts/theme';
 import './styles/global.scss';
 import { ThemeProviderWrapper } from './contexts/ThemeContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProviderWrapper>
-      <ThemeProvider>
-        <RouterProvider router={router}></RouterProvider>
-      </ThemeProvider>
+      <RouterProvider router={router}></RouterProvider>
     </ThemeProviderWrapper>
   </StrictMode>
 );
