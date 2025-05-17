90a92a72e459bda8faa7aeb7a6989c36513b0bf2
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-05-17 10:45:20 -0300

    Feat: add row css partial

--- client/src/styles/abstracts/index.js
+++ client/src/styles/abstracts/index.js
@@ -2,5 +2,6 @@ import * as sizes from './sizes';
 import * as font from './typography';
 import * as utils from './utils';
 import * as variables from './variables';
+import * as mixins from './mixins';
 
-export { sizes, font, utils, variables };
+export { sizes, font, utils, variables, mixins };
