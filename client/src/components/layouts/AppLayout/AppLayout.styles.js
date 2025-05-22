03f9e387e852945baa08398f9db1cf5c828f8135
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-05-22 07:54:50 -0300

    Fix: layout bugs

--- client/src/components/layouts/AppLayout/AppLayout.styles.js
+++ client/src/components/layouts/AppLayout/AppLayout.styles.js
@@ -3,16 +3,16 @@ import { variables } from '@/styles/abstracts';
 
 export const StyledContainer = styled.div`
   width: 100vw;
-  height: 100dvh;
+  height: 100dvh - 5rem;
   max-width: ${variables.appMaxWidth};
   position: relative;
   overflow-x: hidden;
+  margin-bottom: 5rem;
 `;
 
 export const StyledPageContent = styled.div`
   display: flex;
   flex-direction: column;
-  padding-bottom: 5rem;
   background-color: ${({ theme }) => theme.colors.neutral[0]};
   flex: 1;
 `;
