ba0333e4315700e04e0d7e5801d29cc677d015c9
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-05-22 09:35:18 -0300

    Feature: add navigation cards in account

---
+++ client/src/pages/app/Account/Account.styles.js
@@ -0,0 +1,8 @@
+import { utils } from '@/styles/abstracts';
+import styled from 'styled-components';
+
+export const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${utils.rem(40)};
+`;
--- client/src/pages/app/Account/index.jsx
+++ client/src/pages/app/Account/index.jsx
@@ -1,12 +1,35 @@
 import UpdateLayout from '@/components/layouts/PageLayout/components/UpdateLayout';
+import { StyledNavigationCards } from '@/components/containers/StyledNavigationCards';
+import NavigationCard from '@/components/cards/NavigationCard';
+import { UserRoundCog, Landmark } from 'lucide-react';
 import UserAvatar from './components/UserAvatar';
+import { StyledContainer } from './Account.styles';
+
+const BASE_ROUTE = '/app/account';
+const accountOptions = [
+  {
+    label: 'personalDetails',
+    navigateTo: BASE_ROUTE + '/personal-details',
+    Icon: <UserRoundCog />,
+  },
+  {
+    label: 'bankDetails',
+    navigateTo: BASE_ROUTE + '/bank-details',
+    Icon: <Landmark />,
+  },
+];
 
 function Account() {
   return (
-    <div>
-      <UpdateLayout title="Profile" />
+    <StyledContainer>
+      <UpdateLayout title="myAccount" />
       <UserAvatar />
-    </div>
+      <StyledNavigationCards>
+        {accountOptions.map((item) => (
+          <NavigationCard {...item} key={item.label} />
+        ))}
+      </StyledNavigationCards>
+    </StyledContainer>
   );
 }
 
