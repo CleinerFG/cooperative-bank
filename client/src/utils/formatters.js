d57afe75c8d741279e677da17b72d796edb7077b
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-05-26 18:24:10 -0300

    Feat: currency formatter

---
+++ client/src/utils/formatters.js
@@ -0,0 +1,8 @@
+const intlCurrency = new Intl.NumberFormat('pt-BR', {
+  style: 'currency',
+  currency: 'BRL',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+export const currencyFormatter = (value) => intlCurrency.format(value);
