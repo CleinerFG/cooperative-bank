9b65a6f2a39371e73c334f76d0bd8a804a23a56c
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-05-27 17:17:26 -0300

    Fix page layout title font weight

--- client/src/components/layouts/PageLayout/components/Title/Title.styles.js
+++ client/src/components/layouts/PageLayout/components/Title/Title.styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export const StyledTitle = styled.h1`
-  font-weight: ${({ $strong }) => ($strong ? 800 : 600)};
+  font-weight: ${({ $strong }) => ($strong ? 700 : 600)};
 `;
