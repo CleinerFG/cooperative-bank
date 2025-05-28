916d3e85b42741299c092d29c92b459b924ea973
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-05-28 19:16:25 -0300

    Feat: initial loan request page

---
+++ client/src/pages/app/Loans/NewRequest/index.jsx
@@ -0,0 +1,12 @@
+import UpdateLayout from "@/components/layouts/PageLayout/components/UpdateLayout";
+
+function NewRequest() {
+  return (
+    <>
+      <UpdateLayout title="newRequest" />
+      
+    </>
+  );
+}
+
+export default NewRequest;
--- client/src/router/loansRoutes.jsx
+++ client/src/router/loansRoutes.jsx
@@ -1,10 +1,12 @@
 import PageLayout from '@/components/layouts/PageLayout';
 import Loans from '@/pages/app/Loans';
+import NewRequest from '@/pages/app/Loans/NewRequest';
 
 export default {
   path: 'loans',
   element: <PageLayout />,
   children: [
     { index: true, element: <Loans /> },
+    { path: 'new', element: <NewRequest /> },
   ],
 };
