afbceec0603806578a21334c5001998b5ffddb7b
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-05-29 19:11:47 -0300

    Feature: confirm operation modal

---
+++ client/src/components/modals/ConfirmOperationModal/index.jsx
@@ -0,0 +1,40 @@
+import { useTranslation } from 'react-i18next';
+import DataDisplay from '@/components/DataDisplay';
+import Button from '@/components/formElements/Button';
+import Modal from '../Modal';
+import { TriangleAlert } from 'lucide-react';
+import {
+  StyledHeader,
+  StyledIconContainer,
+  StyledTitle,
+  StyledContent,
+  StyledFooter,
+} from '../baseStyles';
+import Input from '@/components/formElements/Input';
+
+function ConfirmOperationModal({ isOpen, onClose, data }) {
+  const { t } = useTranslation();
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <StyledHeader>
+        <StyledIconContainer>
+          <TriangleAlert />
+        </StyledIconContainer>
+        <StyledTitle>{t('confirmOperation')}</StyledTitle>
+      </StyledHeader>
+      <StyledContent>
+        <DataDisplay items={data} />
+        <Input label="operationPass" />
+      </StyledContent>
+      <StyledFooter>
+        <Button variantStyle="secondary" handleClick={onClose}>
+          {t('back')}
+        </Button>
+        <Button>{t('confirm')}</Button>
+      </StyledFooter>
+    </Modal>
+  );
+}
+
+export default ConfirmOperationModal;
