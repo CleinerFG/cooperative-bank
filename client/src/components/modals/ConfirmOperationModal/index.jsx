d0218deac339b98e5c8ab94567c19cdb946a6cc2
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-06-05 17:13:08 -0300

    Fix: form in confirm operation modal

--- client/src/components/modals/ConfirmOperationModal/index.jsx
+++ client/src/components/modals/ConfirmOperationModal/index.jsx
@@ -11,28 +11,46 @@ import {
   StyledFooter,
 } from '../baseStyles';
 import Input from '@/components/formElements/Input';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import validationSchema from './validationSchema';
 
 function ConfirmOperationModal({ isOpen, onClose, data }) {
   const { t } = useTranslation();
+  const { control, handleSubmit } = useForm({
+    resolver: yupResolver(validationSchema),
+    mode: 'onTouched',
+  });
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <StyledHeader>
-        <StyledIconContainer>
-          <TriangleAlert />
-        </StyledIconContainer>
-        <StyledTitle>{t('confirmOperation')}</StyledTitle>
-      </StyledHeader>
-      <StyledContent>
-        <DataDisplay items={data} />
-        <Input label="operationPass" />
-      </StyledContent>
-      <StyledFooter>
-        <Button variant="secondary" handleClick={onClose}>
-          {t('back')}
-        </Button>
-        <Button>{t('confirm')}</Button>
-      </StyledFooter>
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        <StyledHeader>
+          <StyledIconContainer>
+            <TriangleAlert />
+          </StyledIconContainer>
+          <StyledTitle>{t('confirmOperation')}</StyledTitle>
+        </StyledHeader>
+        <StyledContent>
+          <DataDisplay items={data} />
+          <Input
+            control={control}
+            name="operationPass"
+            maskType="numericString"
+            label="operationPass"
+          />
+        </StyledContent>
+        <StyledFooter>
+          <Button variant="secondary" handleClick={onClose}>
+            {t('back')}
+          </Button>
+          <Button type="submit">{t('confirm')}</Button>
+        </StyledFooter>
+      </form>
     </Modal>
   );
 }
