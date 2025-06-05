16e62341cdcd007b20979d1f84d3e3cdb8649b52
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-06-05 16:17:16 -0300

    Feat: RHF useController on select

--- client/src/components/formElements/Select/index.jsx
+++ client/src/components/formElements/Select/index.jsx
@@ -1,24 +1,34 @@
 import { useId } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useController } from 'react-hook-form';
+import FieldError from '../FieldError';
 import { StyledContainer, StyledLabel } from '../baseStyles';
 import { StyledSelect } from './Select.styles';
-import { useTranslation } from 'react-i18next';
 
-function Select({ label, options, selectedOption, onChange }) {
+function Select({ control, name, label, options }) {
   const id = useId();
   const { t } = useTranslation();
+  const { field, fieldState } = useController({ control, name });
+
   return (
     <StyledContainer>
-      <StyledLabel htmlFor={id} onChange={(ev) => onChange(ev.target.value)}>
-        {t(label)}
-      </StyledLabel>
-      <StyledSelect as="select" id={id} value={selectedOption}>
-        <option disabled>{t('selectOp')}</option>
+      <StyledLabel htmlFor={id}>{t(label)}</StyledLabel>
+      <StyledSelect
+        $invalidStyle={fieldState.invalid}
+        as="select"
+        id={id}
+        {...field}
+      >
+        <option disabled value="">
+          {t('selectOp')}
+        </option>
         {options.map((op) => (
           <option value={op.value} key={op.value}>
             {op.text}
           </option>
         ))}
       </StyledSelect>
+      <FieldError error={fieldState.error} />
     </StyledContainer>
   );
 }
