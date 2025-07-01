923fd1d23cd10af628b00412e1861e9aa03772ee
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2025-07-01 13:24:40 -0300

    Fix: image size on modal

--- client/src/components/modals/CropImageModal/CropImageModal.styles.js
+++ client/src/components/modals/CropImageModal/CropImageModal.styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { StyledContent } from '../baseStyles';
+import { sizes } from '@/styles/abstracts';
 
 export const StyledCroppedImage = styled.img`
   width: 100%;
@@ -8,5 +9,39 @@ export const StyledCroppedImage = styled.img`
 
 export const StyledImgContainer = styled(StyledContent)`
   width: 100%;
-  max-width: 500px;
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    max-height: 350px;
+    height: 100%;
+    width: 100%;
+
+    border-radius: ${sizes.rounded.md};
+  }
+
+  .ReactCrop {
+    border-radius: 1rem;
+    overflow: hidden;
+  }
+
+  @media (min-width: 475px) {
+    img {
+      max-height: 450px;
+    }
+  }
+
+  @media (min-width: 575px) {
+    img {
+      max-height: 550px;
+    }
+  }
+
+  @media (min-width: 675px) {
+    img {
+      max-height: 650px;
+    }
+  }
 `;
